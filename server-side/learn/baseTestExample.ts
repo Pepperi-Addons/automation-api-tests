f7fc6301db8b666b27b73f8c8e924a3041e8f6ed
orenv <oren.v@pepperi.com>
2020-10-14 18:00:08 +0300

    Fix for publish addon

--- server-side/learn/baseTestExample.ts
+++ server-side/learn/baseTestExample.ts
@@ -1,55 +1,55 @@
-import Mocha from 'mocha';
-import chai, { expect } from 'chai';
-import promised from 'chai-as-promised';
+// import Mocha from 'mocha';
+// import chai, { expect } from 'chai';
+// import promised from 'chai-as-promised';
 
-chai.use(promised);
+// chai.use(promised);
 
-const mocha = new Mocha();
+// const mocha = new Mocha();
 
-describe('learning about mocha', function () {
-    it('check properties on object', async function () {
-        const f = async () => {
-            return {
-                InternalID: 0,
-                Title: '2',
-                Name: '3',
-            };
-        };
+// describe('learning about mocha', function () {
+//     it('check properties on object', async function () {
+//         const f = async () => {
+//             return {
+//                 InternalID: 0,
+//                 Title: '2',
+//                 Name: '3',
+//             };
+//         };
 
-        return Promise.all([
-            expect(f()).and.eventually.to.include({
-                InternalID: 0,
-                Title: '2',
-            }),
-            expect(f()).and.eventually.to.include({
-                InternalID: 0,
-                Title: '2',
-            }),
-            expect(f()).and.eventually.to.include({
-                InternalID: 0,
-                Title: '3',
-            }),
-            expect(f()).and.eventually.to.include({
-                InternalID: 0,
-                Title: '2',
-            }),
-        ]);
-    });
+//         return Promise.all([
+//             expect(f()).and.eventually.to.include({
+//                 InternalID: 0,
+//                 Title: '2',
+//             }),
+//             expect(f()).and.eventually.to.include({
+//                 InternalID: 0,
+//                 Title: '2',
+//             }),
+//             expect(f()).and.eventually.to.include({
+//                 InternalID: 0,
+//                 Title: '3',
+//             }),
+//             expect(f()).and.eventually.to.include({
+//                 InternalID: 0,
+//                 Title: '2',
+//             }),
+//         ]);
+//     });
 
-    it('check properties on object', function () {
-        const res = [
-            {
-                InternalID: 0,
-                Title: '2',
-                Name: '3',
-            },
-        ];
-        expect(res).to.be.an('array').with.lengthOf(1);
-        expect(res[0]).to.include({
-            InternalID: 0,
-            Title: '2',
-        });
-    });
-});
+//     it('check properties on object', function () {
+//         const res = [
+//             {
+//                 InternalID: 0,
+//                 Title: '2',
+//                 Name: '3',
+//             },
+//         ];
+//         expect(res).to.be.an('array').with.lengthOf(1);
+//         expect(res[0]).to.include({
+//             InternalID: 0,
+//             Title: '2',
+//         });
+//     });
+// });
 
-mocha.run();
+// mocha.run();
