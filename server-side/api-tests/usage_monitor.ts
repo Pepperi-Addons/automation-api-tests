83cc16321fb05c84826cde6308f1c90966c331ec
OlegYef <oleg.y@pepperi.com>
2021-08-30 11:11:24 +0300

    usage monitor updates

---
+++ server-side/api-tests/usage_monitor.ts
@@ -0,0 +1,91 @@
+import GeneralService, { TesterFunctions } from '../services/general.service';
+import { UsageMonitorService } from '../services/usage-monitor.service';
+
+
+export async function UsageMonitorTests(generalService: GeneralService, request, tester: TesterFunctions) {
+    const describe = tester.describe;
+    const expect = tester.expect;
+    const it = tester.it;
+    const usageMonitorService = new UsageMonitorService(generalService);
+
+    const testBaseVersion = '1.0.37';
+
+    //#region Upgrade ADAL
+    const testData = {
+        'Usage Monitor': ['00000000-0000-0000-0000-000000005A9E', '']
+    };
+    const isInstalledArr = await generalService.areAddonsInstalled(testData);
+    const chnageVersionResponseArr = await generalService.chnageVersion(request.body.varKey, testData, false);
+    //#endregion Upgrade ADAL
+
+    describe('addon relation Tests Suites', () => {
+        describe('Prerequisites Addon for relation Tests', () => {
+            //Test Data
+            //ADAL
+            it('Validate that all the needed addons are installed', async () => {
+                isInstalledArr.forEach((isInstalled) => {
+                    expect(isInstalled).to.be.true;
+                });
+            });
+
+            for (const addonName in testData) {
+                const addonUUID = testData[addonName][0];
+                const version = testData[addonName][1];
+                const varLatestVersion = chnageVersionResponseArr[addonName][2];
+                const changeType = chnageVersionResponseArr[addonName][3];
+                describe(`Test Data: ${addonName}`, () => {
+                    it(`${changeType} To Latest Version That Start With: ${version ? version : 'any'}`, () => {
+                        if (chnageVersionResponseArr[addonName][4] == 'Failure') {
+                            expect(chnageVersionResponseArr[addonName][5]).to.include('is already working on version');
+                        } else {
+                            expect(chnageVersionResponseArr[addonName][4]).to.include('Success');
+                        }
+                    });
+
+                    it(`Latest Version Is Installed ${varLatestVersion}`, async () => {
+                        await expect(generalService.papiClient.addons.installedAddons.addonUUID(`${addonUUID}`).get())
+                            .eventually.to.have.property('Version')
+                            .a('string')
+                            .that.is.equal(varLatestVersion);
+                    });
+                });
+            }
+        });
+        describe(`Compare data from base and last versions`, () => {
+            it(`negative : AddonUUID not equale to OwnerID`, async () => {
+                //const secretKey = await relationService.getSecretKey()
+                // console.log(usageMonitorService.papiClient['options'].addonUUID,usageMonitorService.papiClient['options'].addonSecretKey )
+                // usageMonitorService.papiClient['options'].addonUUID = {}
+                // usageMonitorService.papiClient['options'].addonSecretKey = {}
+                // console.log(usageMonitorService.papiClient['options'].addonUUID,usageMonitorService.papiClient['options'].addonSecretKey )
+
+                // debugger
+
+                //usageMonitorService
+               const lastVersion = await usageMonitorService.get();
+                //debugger;
+
+                const tempTestData = {
+                    'Usage Monitor': ['00000000-0000-0000-0000-000000005A9E', testBaseVersion]
+                };
+                await generalService.chnageVersion(request.body.varKey, tempTestData, false);
+                const baseVersion = await usageMonitorService.get();
+                //debugger;
+                delete baseVersion.ExpirationDateTime
+                delete baseVersion.Key
+                delete lastVersion.ExpirationDateTime
+                delete lastVersion.Key
+                delete baseVersion.ExternalData
+                delete lastVersion.ExternalData
+                // oleg2.Setup.Contacts
+                // oleg2.Setup.LicensedUsers
+                expect(lastVersion).to.deep.equal(baseVersion)
+
+                //debugger
+
+            });
+
+            ///////////////////////////////////
+        });
+    });
+}
