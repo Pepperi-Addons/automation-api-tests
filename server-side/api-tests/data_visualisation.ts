d1c1ddeea06f907b782f40b7c7b916362219ac8e
EvgenyVendrovPepperi <evgeny.v@pepperi.com>
2021-09-23 15:00:54 +0300

    data vis is WIP

--- server-side/api-tests/data_visualisation.ts
+++ server-side/api-tests/data_visualisation.ts
@@ -1,16 +1,31 @@
 import GeneralService, { TesterFunctions } from '../services/general.service';
 import { DataVisualisationService, Chart } from '../services/data_visualisation.service';
 
-const name = "test chart";
-const correctType = "Single";
-const wrongType = "not-correct";
 let corrcetScriptUri = "";
-let chart = {
-    "Description": "",
-    "Name": name,
-    "Type": correctType,
-    "ScriptURI": ""
+const possibaleTypes = ["Single", "Series", "MultiSeries"];
+
+const generateRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);
 };
+
+const URL = require("url").URL;
+const stringIsAValidUrl = (s) => {
+    try {
+        new URL(s);
+        return true;
+    } catch (err) {
+        return false;
+    }
+};
+
+let chart: Chart = {
+    "Description": "test chart desc",
+    "Name": "testing chart name",
+    "Type": possibaleTypes[generateRandomNumber(0, 3)],
+    "ScriptURI": "",
+    "ReadOnly": true
+};
+
 export async function DataVisualisationTests(generalService: GeneralService, request, tester: TesterFunctions) {
     const dataVisualisationService = new DataVisualisationService(generalService.papiClient);//just for lint issue
 
@@ -61,16 +76,6 @@ export async function DataVisualisationTests(generalService: GeneralService, req
         });
     });
 
-    const URL = require("url").URL;
-    const stringIsAValidUrl = (s) => {
-        try {
-            new URL(s);
-            return true;
-        } catch (err) {
-            return false;
-        }
-    };
-
     describe('Endpoints', () => {
         describe('GET', () => {
             it('Get Charts - Retriving chart data and validating its format', async () => {
@@ -88,8 +93,8 @@ export async function DataVisualisationTests(generalService: GeneralService, req
                     expect(jsonChartData).to.have.own.property('Type');
                     expect(jsonChartData["Type"]).to.be.oneOf(["Single", "Series", "MultiSeries"]);
                     expect(jsonChartData).to.have.own.property('ScriptURI');
-                    chart["ScriptURI"] = jsonChartData['ScriptURI'];
                     expect(stringIsAValidUrl(jsonChartData['ScriptURI'])).to.equal(true);
+                    corrcetScriptUri = jsonChartData['ScriptURI'];
                     expect(jsonChartData).to.have.own.property('ReadOnly');
                     expect(jsonChartData["ReadOnly"]).to.be.an('Boolean');
                 });
@@ -112,38 +117,56 @@ export async function DataVisualisationTests(generalService: GeneralService, req
                 await testPostEndPointWithoutMandatoryField('Type', dataVisualisationService, generalService, tester);
             });
             it('Upsert chart - with not supported mandatory field Type', async () => {
-                await testPostEndPointWithoutMandatoryField('Type', dataVisualisationService, generalService, tester, wrongType);
+                await testPostEndPointWithoutMandatoryField('Type', dataVisualisationService, generalService, tester, "wrongType");
             });
             it('Upsert chart - w/o mandatory field ScriptURI', async () => {
                 await testPostEndPointWithoutMandatoryField('ScriptURI', dataVisualisationService, generalService, tester);
             });
         });
-    });
-
-    describe('e2e', () => {
-        it('Upsert chart - with valid data - testing output as GET response', async () => {
-            chart["Description"] = "testing chart API";
-            const chartResponse = await dataVisualisationService.postChartAsync(generalService, chart);
-            const chartAuditLogAsync = await generalService.getAuditLogResultObjectIfValid(chartResponse.Body.URI, 40);
-            expect(chartAuditLogAsync["Status"]["Name"]).to.equal('Success');
-            const jsonDataFromAuditLog = JSON.parse(chartAuditLogAsync.AuditInfo.ResultObject);
-            expect(jsonDataFromAuditLog).to.have.own.property('Key');
-            expect(jsonDataFromAuditLog).to.have.own.property('Name');
-            expect(jsonDataFromAuditLog["Name"]).to.equal(chart["Name"]);
-            expect(jsonDataFromAuditLog).to.have.own.property('Description');
-            expect(jsonDataFromAuditLog["Description"]).to.equal(chart["Description"]);
-            expect(jsonDataFromAuditLog).to.have.own.property('Type');
-            expect(jsonDataFromAuditLog["Type"]).to.equal(chart["Type"]);
-            expect(jsonDataFromAuditLog).to.have.own.property('ScriptURI');
-            expect(stringIsAValidUrl(jsonDataFromAuditLog['ScriptURI'])).to.equal(chart["ScriptURI"]);
-            expect(jsonDataFromAuditLog).to.have.own.property('ReadOnly');
-            expect(jsonDataFromAuditLog["ReadOnly"]).to.be.an('Boolean');
+        describe('e2e', () => {
+            it('Upsert chart - with valid data - testing output as GET response & using GET to retrive upstreamed data', async () => {
+                // chart["Description"] = "testing chart API";
+                const listOfChartsToUpsert: Chart[] = createListOfRandCharts();
+                for (let i = 0; i < listOfChartsToUpsert.length; i++) {
+                    const chartResponse = await dataVisualisationService.postChartAsync(generalService, listOfChartsToUpsert[i]);
+                    const chartAuditLogAsync = await generalService.getAuditLogResultObjectIfValid(chartResponse.Body.URI, 40);
+                    expect(chartAuditLogAsync["Status"]["Name"]).to.equal('Success');
+                    const jsonDataFromAuditLog = JSON.parse(chartAuditLogAsync.AuditInfo.ResultObject);
+                    expect(jsonDataFromAuditLog).to.have.own.property('Key');
+                    listOfChartsToUpsert[i]["Key"] = jsonDataFromAuditLog["Key"];
+                    expect(jsonDataFromAuditLog).to.have.own.property('Name');
+                    expect(jsonDataFromAuditLog["Name"]).to.equal(listOfChartsToUpsert[i]["Name"]);
+                    expect(jsonDataFromAuditLog).to.have.own.property('Description');
+                    expect(jsonDataFromAuditLog["Description"]).to.equal(listOfChartsToUpsert[i]["Description"]);
+                    expect(jsonDataFromAuditLog).to.have.own.property('Type');
+                    expect(jsonDataFromAuditLog["Type"]).to.equal(listOfChartsToUpsert[i]["Type"]);
+                    expect(jsonDataFromAuditLog).to.have.own.property('ScriptURI');
+                    listOfChartsToUpsert[i]["ScriptURI"] = jsonDataFromAuditLog["ScriptURI"];
+                    expect(jsonDataFromAuditLog).to.have.own.property('ReadOnly');
+                    expect(jsonDataFromAuditLog["ReadOnly"]).to.be.an('Boolean');
+                    listOfChartsToUpsert[i] = jsonDataFromAuditLog["ReadOnly"];
+                }
+                const chartResponse = await dataVisualisationService.getChartsAsync();
+                const chartAuditLogAsync = await generalService.getAuditLogResultObjectIfValid(chartResponse.URI, 40);
+                expect(chartAuditLogAsync["Status"]["Name"]).to.equal('Success');
+                const jsonDataFromAuditLog = JSON.parse(chartAuditLogAsync.AuditInfo.ResultObject);
+                let upsertedChartFound = 0;
+                jsonDataFromAuditLog.forEach(jsonChartData => {
+                    if (objectsEqual(jsonChartData, listOfChartsToUpsert))
+                        upsertedChartFound++;
+                });
+                expect(upsertedChartFound).to.be.equal(listOfChartsToUpsert.length);
+            });
         });
     });
+
+
 }
+
 
 async function testPostEndPointWithoutMandatoryField(chartFieldToNull, dataVisualisationService, generalService, tester, dataToPass?) {
     const expect = tester.expect;
+    chart["ScriptURI"] = corrcetScriptUri;
     let _chart = { ...chart };
     _chart[chartFieldToNull] = dataToPass ? dataToPass : null;
     const chartResponse = await dataVisualisationService.postChartAsync(generalService, _chart);
@@ -154,7 +177,43 @@ async function testPostEndPointWithoutMandatoryField(chartFieldToNull, dataVisua
     expect(jsonDataFromAuditLog).to.have.own.property('errorMessage');
     expect(jsonDataFromAuditLog.errorMessage).to.include(
         !dataToPass ? `${chartFieldToNull} is a required field` : `${dataToPass} is not supported type`);
-
-
 }
 
+function objectsEqual(o1, listOfCharts) {
+    listOfCharts.array.forEach(element => {
+        if (o1["Key"] === element["Key"] &&
+            o1["Description"] === element["Description"] &&
+            o1["Name"] === element["Name"] &&
+            o1["ReadOnly"] === element["ReadOnly"] &&
+            o1["ScriptURI"] === element["ScriptURI"] &&
+            o1["Type"] !== element["Type"])
+            return true;
+    });
+    return false;
+}
+
+function createListOfRandCharts(): Chart[] {
+    let listOfCharts: Chart[] = [];
+    for (let i = 0; i < 5; i++) {
+        const chartToPush: Chart = {
+            Description: `test-${i}`,
+            Name: `chart-name-${i}`,
+            ReadOnly: false,
+            ScriptURI: corrcetScriptUri,
+            Type: possibaleTypes[i % 3]
+        };
+        listOfCharts.push(chartToPush);
+    }
+    return listOfCharts;
+}
+
+// CreationDateTime?: string;
+// Hidden?: boolean;
+// ModificationDateTime?: string;
+// Description: string;
+// Key:string;
+// Name:string;
+// ReadOnly:boolean;
+// ScriptURI:string;
+// Type: string;
+//["Single", "Series", "MultiSeries"]
--- server-side/services/data_visualisation.service.ts
+++ server-side/services/data_visualisation.service.ts
@@ -2,33 +2,21 @@ import { PapiClient, DataView, FindOptions } from '@pepperi-addons/papi-sdk';
 import GeneralService from './general.service';
 
 export interface Chart {
-    // InternalID?: number;
-    // UUID?: string;
-    // Archive?: boolean;
-    // CreationDateTime?: string;
-    // DeliveryDate?: string;
-    // Hidden?: boolean;
-    // LineNumber: number;
-    // ModificationDateTime?: string;
-    // TotalUnitsPriceAfterDiscount?: number;
-    // TotalUnitsPriceBeforeDiscount?: number;
-    // UnitDiscountPercentage?: number;
-    // UnitPrice?: number;
-    // UnitPriceAfterDiscount?: number;
-    // UnitsQuantity: number;
-    // Item?: NestedObject;
-    // Transaction?: NestedObject;
-
-    [key: string]: any;
+    CreationDateTime?: string;
+    Hidden?: boolean;
+    ModificationDateTime?: string;
+    Key?:string;
+    Description: string;
+    Name:string;
+    ReadOnly:boolean;
+    ScriptURI:string;
+    Type: string;
+    // [key: string]: any;
 }
 
 
 export class DataVisualisationService {
     constructor(public papiClient: PapiClient) { }
-
-    // getDataViewByID(id: number) {
-    //     return this.papiClient.metaData.dataViews.get(id);
-    // }
 
     getCharts(options?: FindOptions) {
         return this.papiClient.get('/addons/api/3d118baf-f576-4cdb-a81e-c2cc9af4d7ad/version/0.0.21/api/charts');
@@ -37,17 +25,6 @@ export class DataVisualisationService {
     getChartsAsync(options?: FindOptions) {
         return this.papiClient.get('/addons/api/async/3d118baf-f576-4cdb-a81e-c2cc9af4d7ad/version/0.0.21/api/charts');
     }
-
-    // getAllDataViews(options?: FindOptions) {
-    //     return this.papiClient.metaData.dataViews.iter(options).toArray();
-    // }
-
-    // postChart(chart: any) {
-    //     // return this.papiClient.metaData.dataViews.upsert(chart);
-    //     return this.papiClient
-    //         .post('/addons/api/async/3d118baf-f576-4cdb-a81e-c2cc9af4d7ad/version/0.0.21/api/charts', chart);
-    //     ;
-    // }
 
     postChartAsync(generalService: GeneralService, chart: any, argHeaders?: any) {
         if (argHeaders)
@@ -64,8 +41,4 @@ export class DataVisualisationService {
                     body: JSON.stringify(chart)
                 });
     }
-
-    // postDataViewBatch(dataViewArr: DataView[]) {
-    //     return this.papiClient.metaData.dataViews.batch(dataViewArr);
-    // }
 }
