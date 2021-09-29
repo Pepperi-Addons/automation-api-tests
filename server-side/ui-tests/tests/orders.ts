ecbc95833a7fd27e9a8a8bcd1d0af96f7d11663c
OrenVildermanPepperi <66742740+OrenVildermanPepperi@users.noreply.github.com>
2021-09-29 23:44:48 +0300

    Improved order tests logs and tested

--- server-side/ui-tests/tests/orders.ts
+++ server-side/ui-tests/tests/orders.ts
@@ -226,113 +226,122 @@ export async function OrdersTest(email: string, password: string, client: Client
         const testDataDiscountPercentage = [0, 5, 10, 20, 50, 75, 100, 200];
         for (let index = 0; index < testDataDiscountPercentage.length; index++) {
             const discount = testDataDiscountPercentage[index];
-            it(`Order The Most Expnsive Three Items With ${discount}% Discount`, async function () {
-                const testDataTransaction: Transaction = await objectsService.createTransaction({
-                    ActivityTypeID: activityTypeId,
-                    UUID: uuidv4(),
-                    Status: 1,
-                    DiscountPercentage: discount,
-                    Account: {
-                        Data: {
-                            InternalID: accountId,
+            describe(`${discount}% Discount Suit`, async function () {
+                it(`Create Transaction With API With ${discount}% Discount`, async function () {
+                    const testDataTransaction: Transaction = await objectsService.createTransaction({
+                        ActivityTypeID: activityTypeId,
+                        UUID: uuidv4(),
+                        Status: 1,
+                        DiscountPercentage: discount,
+                        Account: {
+                            Data: {
+                                InternalID: accountId,
+                            },
                         },
-                    },
-                    Catalog: {
-                        Data: {
-                            InternalID: catalogId,
+                        Catalog: {
+                            Data: {
+                                InternalID: catalogId,
+                            },
                         },
-                    },
-                });
-                transactionId = testDataTransaction.InternalID;
-                transactionUUID = testDataTransaction.UUID;
-            });
-
-            it(`Order The Most Expensive Three Items And Validate ${discount}% Discount`, async function () {
-                const webAppLoginPage = new WebAppTransaction(driver, transactionUUID);
-                await webAppLoginPage.navigate();
-
-                //Sorting items by price
-                const webAppList = new WebAppList(driver);
-                const oren: string[][] = await webAppList.getCartListGridlineAsMatrix();
-                console.table(oren);
-                const sorteByPrice = oren.sort(compareArrayByPriceInDollar);
-                console.table(sorteByPrice);
-
-                addContext(this, {
-                    title: `The items from the UI (soreted by price)`,
-                    value: sorteByPrice,
+                    });
+                    transactionId = testDataTransaction.InternalID;
+                    transactionUUID = testDataTransaction.UUID;
                 });
 
-                const totalPrice =
-                    Number(sorteByPrice[0][2].substring(1)) +
-                    Number(sorteByPrice[1][2].substring(1)) +
-                    Number(sorteByPrice[2][2].substring(1));
-                //Adding most expensive items to cart
-                const webAppTopBar = new WebAppTopBar(driver);
-                for (let i = 0; i < 3; i++) {
-                    await webAppList.sendKeys(webAppTopBar.SearchFieldInput, sorteByPrice[i][1] + Key.ENTER);
-                    await webAppList.sendKysToInputListRowWebElement(0, 1);
-                }
+                it(`Order The Most Expensive Three Items And Validate ${discount}% Discount`, async function () {
+                    const webAppLoginPage = new WebAppTransaction(driver, transactionUUID);
+                    await webAppLoginPage.navigate();
 
-                await webAppList.click(webAppTopBar.CartViewBtn);
-                await webAppList.click(webAppTopBar.CartSumbitBtn);
+                    //Sorting items by price
+                    const webAppList = new WebAppList(driver);
+                    const oren: string[][] = await webAppList.getCartListGridlineAsMatrix();
+                    console.table(oren);
+                    const sorteByPrice = oren.sort(compareArrayByPriceInDollar);
+                    console.table(sorteByPrice);
 
-                //Wait 5 seconds and validate there are no dialogs opening up after placing order
-                //Exmaple on how to write test over a known bug - let the test pass but add information to the report
-                const webAppDialog = new WebAppDialog(driver);
-                try {
-                    await expect(driver.findElement(webAppDialog.Title, 5000, 2)).eventually.to.be.rejectedWith(
-                        'After few retires the maxAttmpts of: 2, Riched: 0',
+                    addContext(this, {
+                        title: `The items from the UI (soreted by price)`,
+                        value: sorteByPrice,
+                    });
+
+                    const totalPrice =
+                        Number(sorteByPrice[0][2].substring(1)) +
+                        Number(sorteByPrice[1][2].substring(1)) +
+                        Number(sorteByPrice[2][2].substring(1));
+                    //Adding most expensive items to cart
+                    const webAppTopBar = new WebAppTopBar(driver);
+                    for (let i = 0; i < 3; i++) {
+                        await webAppList.sendKeys(webAppTopBar.SearchFieldInput, sorteByPrice[i][1] + Key.ENTER);
+                        await webAppList.sendKysToInputListRowWebElement(0, 1);
+                    }
+
+                    await webAppList.click(webAppTopBar.CartViewBtn);
+                    await webAppList.click(webAppTopBar.CartSumbitBtn);
+
+                    //Wait 5 seconds and validate there are no dialogs opening up after placing order
+                    //Exmaple on how to write test over a known bug - let the test pass but add information to the report
+                    const webAppDialog = new WebAppDialog(driver);
+                    try {
+                        await expect(driver.findElement(webAppDialog.Title, 5000, 2)).eventually.to.be.rejectedWith(
+                            'After few retires the maxAttmpts of: 2, Riched: 0',
+                        );
+                    } catch (error) {
+                        const base64Image = await driver.saveScreenshots();
+                        addContext(this, {
+                            title: `This bug happen some time, don't stop the test here, but add this as image(DI-OREN123)`,
+                            value: 'https://pepperi.atlassian.net/browse/DI-17083',
+                        });
+                        addContext(this, {
+                            title: `This bug happen some time, don't stop the test here, but add this as image(DI-OREN123)`,
+                            value: 'data:image/png;base64,' + base64Image,
+                        });
+
+                        //Remove this dialog box and continue the test
+                        await webAppDialog.selectDialogBox('Close');
+                        await driver.sleep(400);
+                        const base64Image2 = await driver.saveScreenshots();
+                        addContext(this, {
+                            title: `Closed the dialog box`,
+                            value: 'data:image/png;base64,' + base64Image2,
+                        });
+                    }
+
+                    //Validating transaction created via the API
+                    const lastTransaction = await objectsService.getTransaction({
+                        order_by: 'ModificationDateTime DESC',
+                    });
+
+                    addContext(this, {
+                        title: `Last transaction lines total price from API`,
+                        value: lastTransaction[0].GrandTotal,
+                    });
+
+                    //Validating price after discount
+                    expect(Number(lastTransaction[0].GrandTotal).toFixed(4)).to.equal(
+                        (totalPrice - (totalPrice / 100) * discount).toFixed(4),
                     );
-                } catch (error) {
-                    const base64Image = await driver.saveScreenshots();
-                    addContext(this, {
-                        title: `This bug happen some time, don't stop the test here, but add this as image(DI-OREN123)`,
-                        value: 'https://pepperi.atlassian.net/browse/DI-17083',
-                    });
-                    addContext(this, {
-                        title: `This bug happen some time, don't stop the test here, but add this as image(DI-OREN123)`,
-                        value: 'data:image/png;base64,' + base64Image,
-                    });
 
-                    //Remove this dialog box and continue the test
-                    await webAppDialog.selectDialogBox('Close');
-                    await driver.sleep(400);
-                    const base64Image2 = await driver.saveScreenshots();
-                    addContext(this, {
-                        title: `Closed the dialog box`,
-                        value: 'data:image/png;base64,' + base64Image2,
+                    //Validating what are the 3 most expensive items via the API are the same as from the UI and ending the tests
+                    const threeMostExpnsiveItems = await objectsService.getItems({
+                        page_size: 3,
+                        order_by: 'Price DESC',
                     });
-                }
+                    const PriceOfMostExpensiveItems = threeMostExpnsiveItems.reduce(
+                        (total, item) => total + Number(item.Price),
+                        0,
+                    );
 
-                //Validating transaction created via the API
-                const lastTransaction = await objectsService.getTransaction({ order_by: 'ModificationDateTime DESC' });
+                    //Validate the correct price of the items from the API and end the test
+                    expect(PriceOfMostExpensiveItems).to.equal(totalPrice);
 
-                addContext(this, {
-                    title: `Last transaction lines total price from API`,
-                    value: lastTransaction[0].GrandTotal,
+                    const webAppHeader = new WebAppHeader(driver);
+                    await expect(webAppHeader.untilIsVisible(webAppHeader.CompanyLogo)).eventually.to.be.true;
                 });
 
-                //Validating price after discount
-                expect(lastTransaction[0].GrandTotal).to.equal(totalPrice - (totalPrice / 100) * discount);
-
-                //Validating what are the 3 most expensive items via the API are the same as from the UI and ending the tests
-                const threeMostExpnsiveItems = await objectsService.getItems({ page_size: 3, order_by: 'Price DESC' });
-                const PriceOfMostExpensiveItems = threeMostExpnsiveItems.reduce(
-                    (total, item) => total + Number(item.Price),
-                    0,
-                );
-
-                //Validate the correct price of the items from the API and end the test
-                expect(PriceOfMostExpensiveItems).to.equal(totalPrice - (totalPrice / 100) * discount);
-
-                const webAppHeader = new WebAppHeader(driver);
-                await expect(webAppHeader.untilIsVisible(webAppHeader.CompanyLogo)).eventually.to.be.true;
-            });
-
-            it('Delete Transaction', async function () {
-                const testDataTransaction = await objectsService.deleteTransaction(transactionId);
-                expect(testDataTransaction).to.be.true;
+                it('Delete Transaction', async function () {
+                    const testDataTransaction = await objectsService.deleteTransaction(transactionId);
+                    expect(testDataTransaction).to.be.true;
+                });
             });
         }
     });
