f7234316d685bf0c7a16ba4a878700623f651f39
OferPepperi <ofer.l@pepperi.com>
2022-01-06 12:48:50 +0200

    removing commented code

--- server-side/api-tests/objects/pages/pages.ts
+++ server-side/api-tests/objects/pages/pages.ts
@@ -9,7 +9,7 @@ export async function PagesTestSuite(generalService: GeneralService, tester: Tes
     const expect = tester.expect;
     const it = tester.it;
     const pagesService = new PagesService(generalService);
-    // const pageBlockRelation = await pagesService.getBlockRelation('Page Block Tester');
+
     describe('Pages API Tests Suite', () => {
         const expected: Page = {
             Name: `${new Date().toLocaleDateString()} - PagesApiTest`,
@@ -26,16 +26,6 @@ export async function PagesTestSuite(generalService: GeneralService, tester: Tes
         });
 
         it('Page Blocks Test Suite', async () => await PageBlocksTestSuite(pagesService, tester, expected));
-        // it('Page blocks', async () => await PageBlocksTestSuite(pagesService, tester, expected
-        //     // {
-        //     //     Name: `${new Date().toLocaleDateString()} - PagesApiTest`,
-        //     //     Blocks: [],
-        //     //     Layout: {
-        //     //         Sections: [
-        //     //         ],
-        //     //     },
-        //     // }
-        // ));
 
         // it('Change section data', async () => {
         //     expected.Layout.Sections[0].Name = 'Test Section';
@@ -49,47 +39,8 @@ export async function PagesTestSuite(generalService: GeneralService, tester: Tes
             expect(resultPage.Hidden).to.equal(true);
         });
 
-        // function comparePages(
-        //     expected: any,
-        //     actual: any,
-        //     expect : Chai.ExpectStatic,
-        //     excludedProperties: Array<string> = ['length'],
-        //     parentProp?: string,
-        // ) {
-        //     if (typeof expected === 'object') {
-        //         const properties = Object.getOwnPropertyNames(expected).filter((prop) =>
-        //             excludedProperties && excludedProperties.length > 0 ? !excludedProperties.includes(prop) : prop,
-        //         );
-
-        //         properties.forEach((prop) => {
-        //             parentProp = parentProp ? parentProp + '.' + prop : prop;
-        //             if (typeof expected[prop] === 'object') {
-        //                 comparePages(expected[prop], actual[prop], expect, excludedProperties, parentProp);
-        //             } else {
-        //                 pagesService.assertAndLog(expected[prop], actual[prop], expect, parentProp);
-        //             }
-        //         });
-        //     } else {
-        //         pagesService.assertAndLog(expected, actual, expect);
-        //     }
-        // }
+        
     });
 
-    // function AssertAndLog(expected: any, actual: any, parentProp = ''): void {
-    //     console.log(`Expected ${parentProp}: ${typeof expected === 'object' ? JSON.stringify(expected) : expected}`);
-    //     console.log(`Actual ${parentProp}: ${typeof actual === 'object' ? JSON.stringify(actual) : actual}`);
-    //     expect(
-    //         actual,
-    //         `The objects don't match: \nExpected: ${JSON.stringify(expected)}\nActual: ${JSON.stringify(actual)}`,
-    //     ).to.equal(expected);
-    // }
 }
 
-// export function AssertAndLog(expected: any, actual: any, expect : Chai.ExpectStatic, parentProp = ''): void {
-//     console.log(`Expected ${parentProp}: ${typeof expected === 'object' ? JSON.stringify(expected) : expected}`);
-//     console.log(`Actual ${parentProp}: ${typeof actual === 'object' ? JSON.stringify(actual) : actual}`);
-//     expect(
-//         actual,
-//         `The objects don't match: \nExpected: ${JSON.stringify(expected)}\nActual: ${JSON.stringify(actual)}`,
-//     ).to.equal(expected);
-// }
