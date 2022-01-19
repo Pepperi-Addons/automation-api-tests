a46e1cc372f7926e076cbdf22a90b45a3de5ab24
PEPPERI\evgeny.v <evgeny.v@pepperi.com>
2022-01-19 17:31:28 +0200

    comments deleted

--- server-side/ui-tests/tests/uom.ts
+++ server-side/ui-tests/tests/uom.ts
@@ -11,8 +11,6 @@ import { By } from 'selenium-webdriver';
 
 chai.use(promised);
 
-//npm run ui-show-report --server=prod --chrome_headless=false --user_email='UOM_UI@pepperitest.com' --user_pass='Aa1234567' --var_pass='Basic dmFyM0BwZXBwZXJpLmNvbTpQZXAxMjNRYXc='
-
 export async function UomTests(email: string, password: string, varPass: string, client: Client) {
     const generalService = new GeneralService(client);
     const objectsService = new ObjectsService(generalService);
@@ -71,43 +69,43 @@ export async function UomTests(email: string, password: string, varPass: string,
 
             describe('Data Preparation Using Endpoints', () => {
 
-                // it('Post items for uom', async function () {
-                //     const itemsToPost = createItemsList();
-                //     const postItemsResponse: Item[] = [];
-                //     for (let i = 0; i < itemsToPost.length; i++) {
-                //         postItemsResponse.push(await objectsService.postItem(itemsToPost[i]));
-                //     }
+                it('Post items for uom', async function () {
+                    const itemsToPost = createItemsList();
+                    const postItemsResponse: Item[] = [];
+                    for (let i = 0; i < itemsToPost.length; i++) {
+                        postItemsResponse.push(await objectsService.postItem(itemsToPost[i]));
+                    }
 
-                //     for (let i = 0; i < postItemsResponse.length; i++) {
-                //         expect(postItemsResponse[i].ExternalID).to.equal(itemsToPost[i].ExternalID);
-                //         expect(postItemsResponse[i].MainCategoryID).to.equal(itemsToPost[i].MainCategoryID);
-                //         expect(postItemsResponse[i].Name).to.equal(itemsToPost[i].Name);
-                //         expect(postItemsResponse[i].Price).to.equal(itemsToPost[i].Price);
-                //     }
-                // });
+                    for (let i = 0; i < postItemsResponse.length; i++) {
+                        expect(postItemsResponse[i].ExternalID).to.equal(itemsToPost[i].ExternalID);
+                        expect(postItemsResponse[i].MainCategoryID).to.equal(itemsToPost[i].MainCategoryID);
+                        expect(postItemsResponse[i].Name).to.equal(itemsToPost[i].Name);
+                        expect(postItemsResponse[i].Price).to.equal(itemsToPost[i].Price);
+                    }
+                });
 
-                // it('Post items: UOM fields', async function () {
-                //     const uomItemsToPost: UomItem[] = createUomItemsList();
-                //     const postUomItemsResponse: any[] = [];
-                //     for (let i = 0; i < uomItemsToPost.length; i++) {
-                //         postUomItemsResponse.push(
-                //             await generalService.fetchStatus
-                //                 (
-                //                     `/addons/api/1238582e-9b32-4d21-9567-4e17379f41bb/api/uoms`,
-                //                     {
-                //                         method: 'POST',
-                //                         body: JSON.stringify(uomItemsToPost[i]),
-                //                     }
-                //                 )
-                //         );
-                //     }
-                //     for (let i = 0; i < postUomItemsResponse.length; i++) {
-                //         expect(postUomItemsResponse[i].Status).to.equal(200);
-                //         expect(postUomItemsResponse[i].Body.Key).to.equal(uomItemsToPost[i].Key);
-                //         expect(postUomItemsResponse[i].Body.Multiplier).to.equal(uomItemsToPost[i].Multiplier);
-                //         expect(postUomItemsResponse[i].Body.Title).to.equal(uomItemsToPost[i].Title);
-                //     }
-                // });
+                it('Post items: UOM fields', async function () {
+                    const uomItemsToPost: UomItem[] = createUomItemsList();
+                    const postUomItemsResponse: any[] = [];
+                    for (let i = 0; i < uomItemsToPost.length; i++) {
+                        postUomItemsResponse.push(
+                            await generalService.fetchStatus
+                                (
+                                    `/addons/api/1238582e-9b32-4d21-9567-4e17379f41bb/api/uoms`,
+                                    {
+                                        method: 'POST',
+                                        body: JSON.stringify(uomItemsToPost[i]),
+                                    }
+                                )
+                        );
+                    }
+                    for (let i = 0; i < postUomItemsResponse.length; i++) {
+                        expect(postUomItemsResponse[i].Status).to.equal(200);
+                        expect(postUomItemsResponse[i].Body.Key).to.equal(uomItemsToPost[i].Key);
+                        expect(postUomItemsResponse[i].Body.Multiplier).to.equal(uomItemsToPost[i].Multiplier);
+                        expect(postUomItemsResponse[i].Body.Title).to.equal(uomItemsToPost[i].Title);
+                    }
+                });
 
                 describe('UI Tests', () => {
                     this.retries(1);
@@ -122,26 +120,26 @@ export async function UomTests(email: string, password: string, varPass: string,
                         await driver.quit();
                     });
 
-                    // it('Set Up UOM ATD', async function () {
-                    //     const webAppLoginPage = new WebAppLoginPage(driver);
-                    //     await webAppLoginPage.loginNoCompanyLogo(email, password);
-                    //     //1. validating all items are added to the main catalog
-                    //     const addonPage = new AddonPage(driver);
-                    //     await addonPage.selectCatalogItemsByCategory("uom item", "NOT uom item");
-                    //     //2. goto ATD editor - create new ATD UOM_{random-hashstring}
-                    //     await addonPage.createNewATD(this, generalService, _TEST_DATA_ATD_NAME, _TEST_DATA_ATD_DESCRIPTION);
-                    //     //3. goto new ATD and configure everything needed for the test - 3 calculated fields 
-                    //     //3.1.configure Allowed UOMs Field as AllowedUomFieldsForTest, UOM Configuration Field as ItemConfig and uom data field as ConstInventory
-                    //     //3.2. add fields to UI control of ATD
-                    //     await addonPage.configUomATD();
-                    //     await addonPage.returnToHomePage();
-                    //     await addonPage.openSettings();
-                    //     //4. add the ATD to home screen
-                    //     await addonPage.addAdminHomePageButtons(_TEST_DATA_ATD_NAME);
-                    //     const webAppHomePage = new WebAppHomePage(driver);
-                    //     await webAppHomePage.manualResync();
-                    //     await webAppHomePage.validateATDIsApearingOnHomeScreen(_TEST_DATA_ATD_NAME);
-                    // });
+                    it('Set Up UOM ATD', async function () {
+                        const webAppLoginPage = new WebAppLoginPage(driver);
+                        await webAppLoginPage.loginNoCompanyLogo(email, password);
+                        //1. validating all items are added to the main catalog
+                        const addonPage = new AddonPage(driver);
+                        await addonPage.selectCatalogItemsByCategory("uom item", "NOT uom item");
+                        //2. goto ATD editor - create new ATD UOM_{random-hashstring}
+                        await addonPage.createNewATD(this, generalService, _TEST_DATA_ATD_NAME, _TEST_DATA_ATD_DESCRIPTION);
+                        //3. goto new ATD and configure everything needed for the test - 3 calculated fields 
+                        //3.1.configure Allowed UOMs Field as AllowedUomFieldsForTest, UOM Configuration Field as ItemConfig and uom data field as ConstInventory
+                        //3.2. add fields to UI control of ATD
+                        await addonPage.configUomATD();
+                        await addonPage.returnToHomePage();
+                        await addonPage.openSettings();
+                        //4. add the ATD to home screen
+                        await addonPage.addAdminHomePageButtons(_TEST_DATA_ATD_NAME);
+                        const webAppHomePage = new WebAppHomePage(driver);
+                        await webAppHomePage.manualResync();
+                        await webAppHomePage.validateATDIsApearingOnHomeScreen(_TEST_DATA_ATD_NAME);
+                    });
                     it('UI Test UOM ATD', async function () {
                         const webAppLoginPage = new WebAppLoginPage(driver);
                         await webAppLoginPage.loginNoCompanyLogo(email, password);
@@ -194,24 +192,24 @@ export async function UomTests(email: string, password: string, varPass: string,
                             }
                         });
                     });
-                    // it('Delete test ATD from dist + home screen using UI', async function () {
-                    //     const webAppLoginPage = new WebAppLoginPage(driver);
-                    //     await webAppLoginPage.loginNoCompanyLogo(email, password);
-                    //     const addonPage = new AddonPage(driver);
-                    //     await addonPage.openSettings();
-                    //     await addonPage.removeAdminHomePageButtons(_TEST_DATA_ATD_NAME);
-                    //     await addonPage.removeATD(generalService, _TEST_DATA_ATD_NAME, _TEST_DATA_ATD_DESCRIPTION);//maybe should be done using API
-                    // });
+                    it('Delete test ATD from dist + home screen using UI', async function () {
+                        const webAppLoginPage = new WebAppLoginPage(driver);
+                        await webAppLoginPage.loginNoCompanyLogo(email, password);
+                        const addonPage = new AddonPage(driver);
+                        await addonPage.openSettings();
+                        await addonPage.removeAdminHomePageButtons(_TEST_DATA_ATD_NAME);
+                        await addonPage.removeATD(generalService, _TEST_DATA_ATD_NAME, _TEST_DATA_ATD_DESCRIPTION);//maybe should be done using API
+                    });
                 });
                 describe('Test Data Cleansing using API', () => {
-                    // it('Reset Existing Items', async function () {
-                    //     //Remove all items
-                    //     const itemsArr = await generalService.papiClient.items.find({ page_size: -1 });
-                    //     for (let i = 0; i < itemsArr.length; i++) {
-                    //         const deleted = await generalService.papiClient.items.delete(itemsArr[i].InternalID as number);
-                    //         expect(deleted).to.be.true;
-                    //     }
-                    // });
+                    it('Reset Existing Items', async function () {
+                        //Remove all items
+                        const itemsArr = await generalService.papiClient.items.find({ page_size: -1 });
+                        for (let i = 0; i < itemsArr.length; i++) {
+                            const deleted = await generalService.papiClient.items.delete(itemsArr[i].InternalID as number);
+                            expect(deleted).to.be.true;
+                        }
+                    });
                 });
             });
 
