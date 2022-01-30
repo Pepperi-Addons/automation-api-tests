4583cd37c748335fdab9f53332d95aaa39a129c3
PEPPERI\evgeny.v <evgeny.v@pepperi.com>
2022-01-30 15:19:06 +0200

    better logging of searching element

--- server-side/ui-tests/utilities/browser.ts
+++ server-side/ui-tests/utilities/browser.ts
@@ -109,8 +109,7 @@ export class Browser {
                             `document.evaluate("${selector['value']}", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null).snapshotItem(${index}).click();`,
                         );
                         console.log(
-                            `%cClicked with xpath selector: ${
-                                selector.valueOf()['value']
+                            `%cClicked with xpath selector: ${selector.valueOf()['value']
                             }, on element with index of: ${index}`,
                             ConsoleColors.ClickedMessage,
                         );
@@ -119,8 +118,7 @@ export class Browser {
                             `document.querySelectorAll("${selector['value']}")[${index}].click();`,
                         );
                         console.log(
-                            `%cClicked with css selector: ${
-                                selector.valueOf()['value']
+                            `%cClicked with css selector: ${selector.valueOf()['value']
                             }, on element with index of: ${index}`,
                             ConsoleColors.ClickedMessage,
                         );
@@ -154,8 +152,7 @@ export class Browser {
             this.sleep(400);
             await (await this.findElements(selector, waitUntil))[index].sendKeys(keys);
             console.log(
-                `%cSentKeys with defult selector: ${
-                    selector.valueOf()['value']
+                `%cSentKeys with defult selector: ${selector.valueOf()['value']
                 }, on element with index of: ${index}, Keys: '${isSecret ? '******' : keys}'`,
                 ConsoleColors.SentKeysMessage,
             );
@@ -174,8 +171,7 @@ export class Browser {
                         await this.driver.actions().keyDown(Key.CONTROL).sendKeys('a').keyUp(Key.CONTROL).perform();
                         await el[index].sendKeys(keys);
                         console.log(
-                            `%cSentKeys with actions and defult selector: ${
-                                selector.valueOf()['value']
+                            `%cSentKeys with actions and defult selector: ${selector.valueOf()['value']
                             }, on element with index of: ${index}, Keys: '${isSecret ? '******' : keys}'`,
                             ConsoleColors.SentKeysMessage,
                         );
@@ -185,8 +181,7 @@ export class Browser {
                                 `document.evaluate("${selector['value']}", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null).snapshotItem(${index}).value='${keys}';`,
                             );
                             console.log(
-                                `%cSet value with xpath selector: ${
-                                    selector.valueOf()['value']
+                                `%cSet value with xpath selector: ${selector.valueOf()['value']
                                 }, on element with index of: ${index}, Keys: '${isSecret ? '******' : keys}'`,
                                 ConsoleColors.SentKeysMessage,
                             );
@@ -195,8 +190,7 @@ export class Browser {
                                 `document.querySelectorAll("${selector['value']}")[${index}].value='${keys}';`,
                             );
                             console.log(
-                                `%cSet value with css selector: ${
-                                    selector.valueOf()['value']
+                                `%cSet value with css selector: ${selector.valueOf()['value']
                                 }, on element with index of: ${index}, Keys: '${isSecret ? '******' : keys}'`,
                                 ConsoleColors.SentKeysMessage,
                             );
@@ -255,7 +249,7 @@ export class Browser {
                 `After wait time of: ${waitUntil}, for selector of ${selector['value']}, The test must end, The element is not visible`,
             );
         } else {
-            console.log(`element with selector ${selector.valueOf()['value']} is found successfully`);
+            console.log(`element with selector: '${selector.valueOf()['value']}' is found successfully`);
         }
         return elArr;
     }
