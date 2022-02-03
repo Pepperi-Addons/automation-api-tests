b666c69d3b8e8210634092d97aa50194d7f99465
PEPPERI\evgeny.v <evgeny.v@pepperi.com>
2022-02-03 17:16:58 +0200

    sync + fix to uom

--- server-side/ui-tests/pom/WebAppAPI.ts
+++ server-side/ui-tests/pom/WebAppAPI.ts
@@ -7,12 +7,54 @@ import GeneralService from '../../services/general.service';
 export class WebAppAPI extends Page {
     table: string[][] = [];
     _CLIENT: Client;
-    _IS_STAGE: boolean;
-    webAPIVersionVersion = '0.0.0';
+    _BASE_URL: string;
     constructor(browser: Browser, client: Client) {
         super(browser, `${config.baseUrl}`);
         this._CLIENT = client;
-        this._IS_STAGE = client.BaseURL.includes('staging');
+        this._BASE_URL = '';
+    }
+
+    async getSyncResponse(accessToken: string, loopsAmount = 30) {
+        const generalService = new GeneralService(this._CLIENT);
+        let syncStatusReposnse;
+        const url = `${this._BASE_URL === '' ? await this.getBaseURL() : this._BASE_URL}/Service1.svc/v1/GetSyncStatus`;
+        do {
+            syncStatusReposnse = await generalService.fetchStatus(url, {
+                method: 'GET',
+                headers: {
+                    PepperiSessionToken: accessToken,
+                    'Content-Type': 'application/json',
+                },
+            });
+            //This case is used when syncStatus was not created at all
+            syncStatusReposnse = syncStatusReposnse.Body;
+            if (syncStatusReposnse === null) {
+                this.browser.sleep(5000);
+                console.log('Sync status not found, waiting...');
+            }
+            //This case will only retry the get call again as many times as the "loopsAmount"
+            else if (syncStatusReposnse.Status == 'Processing') {
+                await this.browser.sleep(5000);
+                console.log(`UpToDate: Retry ${loopsAmount} Times.`);
+            }
+            loopsAmount--;
+        } while ((syncStatusReposnse === null || syncStatusReposnse.Status == 'Processing') && loopsAmount > 0);
+        return syncStatusReposnse;
+    }
+
+    async initSync(accessToken: string) {
+        //webapi.sandbox.pepperi.com/16.60.82/webapi/Service1.svc/v1/HomePage
+        const URL = `${this._BASE_URL === '' ? await this.getBaseURL() : this._BASE_URL}/Service1.svc/v1/HomePage`;
+        const navigateToHomescreen = await (
+            await fetch(URL, {
+                method: 'GET',
+                headers: {
+                    PepperiSessionToken: accessToken,
+                    'Content-Type': 'application/json',
+                },
+            })
+        ).json();
+        return navigateToHomescreen;
     }
 
     /**
@@ -21,16 +63,12 @@ export class WebAppAPI extends Page {
      */
     public async getAccessToken(): Promise<string> {
         const generalService = new GeneralService(this._CLIENT);
-        const webAPIVersion = await generalService.getAddonsByUUID('00000000-0000-0000-0000-0000003eba91');
-        this.webAPIVersionVersion = webAPIVersion.Version as string;
         let createSessionResponse;
         let maxLoopsCounter = 90;
         do {
             generalService.sleep(2000);
             createSessionResponse = await generalService.fetchStatus(
-                `https://webapi${this._IS_STAGE ? '.sandbox.' : '.'}pepperi.com/${
-                    this.webAPIVersionVersion
-                }/webapi/Service1.svc/v1/CreateSession`,
+                `${this._BASE_URL === '' ? await this.getBaseURL() : this._BASE_URL}/Service1.svc/v1/CreateSession`,
                 {
                     method: 'POST',
                     body: JSON.stringify({
@@ -60,9 +98,9 @@ export class WebAppAPI extends Page {
         do {
             generalService.sleep(2000);
             searchResponse = await generalService.fetchStatus(
-                `https://webapi${this._IS_STAGE ? '.sandbox.' : '.'}pepperi.com/${
-                    this.webAPIVersionVersion
-                }/webapi/Service1.svc/v1/Cart/Transaction/${catalogUUID}/Items/Search`,
+                `${
+                    this._BASE_URL === '' ? await this.getBaseURL() : this._BASE_URL
+                }/Service1.svc/v1/Cart/Transaction/${catalogUUID}/Items/Search`,
                 {
                     method: 'POST',
                     body: JSON.stringify({
@@ -98,9 +136,9 @@ export class WebAppAPI extends Page {
         do {
             generalService.sleep(2000);
             searchResponse = await generalService.fetchStatus(
-                `https://webapi${this._IS_STAGE ? '.sandbox.' : '.'}pepperi.com/${
-                    this.webAPIVersionVersion
-                }/webapi/Service1.svc/v1/Cart/Transaction/${catalogUUID}`,
+                `${
+                    this._BASE_URL === '' ? await this.getBaseURL() : this._BASE_URL
+                }/Service1.svc/v1/Cart/Transaction/${catalogUUID}`,
                 {
                     method: 'GET',
                     headers: {
@@ -112,5 +150,11 @@ export class WebAppAPI extends Page {
             maxLoopsCounter--;
         } while (searchResponse.Ok == null && maxLoopsCounter > 0);
         return searchResponse.Body;
+    }
+
+    public async getBaseURL() {
+        const generalService = new GeneralService(this._CLIENT);
+        this._BASE_URL = await (await generalService.papiClient.get('/webapi/base_url')).BaseURL;
+        return this._BASE_URL;
     }
 }
