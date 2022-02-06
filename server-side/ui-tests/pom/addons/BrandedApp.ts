a558e2ca38db761d55d824d6017e2bab13e85aff
PEPPERI\evgeny.v <evgeny.v@pepperi.com>
2022-02-06 15:22:11 +0200

    changed branded app 'super' to 'this'

--- server-side/ui-tests/pom/addons/BrandedApp.ts
+++ server-side/ui-tests/pom/addons/BrandedApp.ts
@@ -20,17 +20,17 @@ export class BrandedApp extends AddonPage {
         //keep for now
         const webAppSettingsSidePanel = new WebAppSettingsSidePanel(this.browser);
         await webAppSettingsSidePanel.selectSettingsByID('Company Profile');
-        await super.browser.click(webAppSettingsSidePanel.SettingsFrameworkHomeButtons);
+        await this.browser.click(webAppSettingsSidePanel.SettingsFrameworkHomeButtons);
 
-        await super.isSpinnerDone();
-        await super.browser.switchTo(this.AddonContainerIframe);
-        await super.isAddonFullyLoaded(AddonLoadCondition.Content);
+        await this.isSpinnerDone();
+        await this.browser.switchTo(this.AddonContainerIframe);
+        await this.isAddonFullyLoaded(AddonLoadCondition.Content);
 
-        await super.browser.click(this.AddonContainerEditAdmin);
-        await super.browser.sendKeys(this.SettingsFrameworkEditorSearch, activtiyName + Key.ENTER);
-        await super.browser.click(this.AddonContainerEditorSave);
+        await this.browser.click(this.AddonContainerEditAdmin);
+        await this.browser.sendKeys(this.SettingsFrameworkEditorSearch, activtiyName + Key.ENTER);
+        await this.browser.click(this.AddonContainerEditorSave);
 
-        await super.browser.switchToDefaultContent();
+        await this.browser.switchToDefaultContent();
         const webAppHomePage = new WebAppHomePage(this.browser);
         await webAppHomePage.returnToHomePage();
         return;
@@ -45,32 +45,32 @@ export class BrandedApp extends AddonPage {
         //keep for now
         const webAppSettingsSidePanel = new WebAppSettingsSidePanel(this.browser);
         await webAppSettingsSidePanel.selectSettingsByID('Company Profile');
-        await super.browser.click(webAppSettingsSidePanel.SettingsFrameworkHomeButtons);
+        await this.browser.click(webAppSettingsSidePanel.SettingsFrameworkHomeButtons);
 
-        await super.isSpinnerDone();
-        await super.browser.switchTo(this.AddonContainerIframe);
-        await super.isAddonFullyLoaded(AddonLoadCondition.Content);
+        await this.isSpinnerDone();
+        await this.browser.switchTo(this.AddonContainerIframe);
+        await this.isAddonFullyLoaded(AddonLoadCondition.Content);
 
-        await super.browser.click(this.AddonContainerEditAdmin);
+        await this.browser.click(this.AddonContainerEditAdmin);
 
         const buttonsLocator = Object.assign({}, this.AddonContainerEditorTrashBtn);
         buttonsLocator['value'] = buttonsLocator['value'].replace('ATD_PLACE_HOLDER', activtiyName);
 
         let isRemovable;
         try {
-            isRemovable = await super.browser.untilIsVisible(buttonsLocator);
+            isRemovable = await this.browser.untilIsVisible(buttonsLocator);
         } catch (error) {
             console.log('%cNo Button To Remove, Test Continue', ConsoleColors.PageMessage);
         }
         if (isRemovable) {
-            const buttonsToRemove = await super.browser.findElements(buttonsLocator);
+            const buttonsToRemove = await this.browser.findElements(buttonsLocator);
             for (let i = 0; i < buttonsToRemove.length; i++) {
-                await super.browser.click(buttonsLocator);
+                await this.browser.click(buttonsLocator);
             }
-            await super.browser.click(this.AddonContainerEditorSave);
+            await this.browser.click(this.AddonContainerEditorSave);
         }
 
-        const webAppHomePage = new WebAppHomePage(super.browser);
+        const webAppHomePage = new WebAppHomePage(this.browser);
         await webAppHomePage.returnToHomePage();
         return;
     }
