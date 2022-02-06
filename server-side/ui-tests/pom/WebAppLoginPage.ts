bb73fcd6e2985b5167c0ef9368504fceb2f6d400
PEPPERI\evgeny.v <evgeny.v@pepperi.com>
2022-02-06 10:22:35 +0200

    revreted to 'Pages'

--- server-side/ui-tests/pom/WebAppLoginPage.ts
+++ server-side/ui-tests/pom/WebAppLoginPage.ts
@@ -1,5 +1,5 @@
 import { Browser } from '../utilities/browser';
-import { Page } from './base/page';
+import { Page } from './base/Page';
 import config from '../../config';
 import { By, Locator } from 'selenium-webdriver';
 import chai, { expect } from 'chai';
