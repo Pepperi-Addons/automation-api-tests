046c86b58072a8497630a7d8457c337f9824a1f6
PEPPERI\evgeny.v <evgeny.v@pepperi.com>
2022-02-15 09:26:38 +0200

    small fix for prod UOM

--- server-side/ui-tests/pom/OrderPage.ts
+++ server-side/ui-tests/pom/OrderPage.ts
@@ -11,7 +11,7 @@ export class OrderPage extends WebAppList {
     public async changeOrderCenterPageView(viewType: string) {
         //switch to medium view:
         //1. click on btn to open drop down
-        this.clickViewMenu();
+        await this.clickViewMenu();
         //2. pick wanted view
         const injectedViewType = this.ViewTypeOption.valueOf()['value'].slice().replace('|textToFill|', viewType);
         await this.browser.click(By.xpath(injectedViewType));
