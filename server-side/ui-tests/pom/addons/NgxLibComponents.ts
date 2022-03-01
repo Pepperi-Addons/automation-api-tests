7be116364e86b266a0d6cf0eb5359fc7cc7546d7
PEPPERI\evgeny.v <evgeny.v@pepperi.com>
2022-03-01 15:38:08 +0200

    checkbox finished + refactored

--- server-side/ui-tests/pom/addons/NgxLibComponents.ts
+++ server-side/ui-tests/pom/addons/NgxLibComponents.ts
@@ -22,6 +22,14 @@ export class NgxLibComponents extends AddonPage {
     public formTitle: Locator = By.xpath('//*[@class="mat-form-title"]');
     public noFileTitle: Locator = By.xpath('//*[contains(text(),"Drag and drop a file here or click")]');
     public openSrcButton: Locator = By.xpath("//*[@class='pep-file-preview ng-star-inserted']");
+    public checkBoxLabel: Locator = By.xpath("//span[@class='mat-checkbox-label']//span[2]");
+    public checkBoxValueEmoji: Locator = By.xpath("//div[contains(@title,'checkbox')]");
+    public checkBoxValueCheck: Locator = By.xpath("//input[@type='checkbox']");
+    public checkBoxFieldTitle: Locator = By.css("pep-field-title > div");
+    public checkBoxComponent: Locator = By.css("pep-checkbox");
+    public checkBoxEmoji: Locator = By.xpath("//button[contains(@class,'emoji-icon')]");
+    public checkBoxEmojiTitle: Locator = By.xpath("//span[contains(@class,'emoji-title')]");
+    //->
 
 
 
@@ -180,7 +188,7 @@ export class NgxLibComponents extends AddonPage {
                 break;
         }
         const expectedH = typeOfComponent === "Button" ? expectedComponentClassesSplited[indexToGetDataFrom].split(delimiterToSplitBy)[0] : (parsedExpectedData[indexToGetDataFrom] as string).split(delimiterToSplitBy)[1].split('x')[0];
-        const expectedW = typeOfComponent === "Button" ? expectedComponentClassesSplited[indexToGetDataFrom].split(delimiterToSplitBy)[0] : (parsedExpectedData[indexToGetDataFrom] as string).split(delimiterToSplitBy)[1].split('x')[1];
+        const expectedW = typeOfComponent === "Button" ? expectedComponentClassesSplited[indexToGetDataFrom].split(delimiterToSplitBy)[1] : (parsedExpectedData[indexToGetDataFrom] as string).split(delimiterToSplitBy)[1].split('x')[1];
         return { height: parseInt(expectedH), width: parseInt(expectedW), x: 0, y: 0 } as IRectangle;
     }
 
@@ -229,4 +237,105 @@ export class NgxLibComponents extends AddonPage {
         await this.browser.click(this.nextTestBtn);
         this.browser.sleep(2000);
     }
+
+    /**
+     *
+     *
+     */
+    public async getCheckBoxValue(type: string): Promise<boolean> {
+        const checkBoxValue = type === "checkbox" ? (await (await this.browser.findElement(this.checkBoxValueCheck)).getAttribute("aria-checked")) :
+            (await (await this.browser.findElement(this.checkBoxValueEmoji)).getAttribute("title")).split(":")[1].trim().toLowerCase();
+        return checkBoxValue === "true" ? true : false;
+    }
+
+    /**
+    *
+    *
+    */
+    public async getCheckBoxLabelText(type: string): Promise<string> {
+        return type === "checkbox" ? await (await this.browser.findElement(this.checkBoxLabel)).getText() : await (await this.browser.findElement(this.checkBoxEmojiTitle)).getText();
+    }
+
+    /**
+    *
+    *
+    */
+    public async getIfCheckBoxShown(type: string, value: boolean, isDisabled: boolean): Promise<boolean> {
+        if (type === "checkbox") {
+            return await this.testIfElementShown(this.checkBoxValueCheck);
+        } else {
+            if (!(await this.testIfElementShown(this.checkBoxEmoji))) {
+                return false;
+            }
+            const checkBoxEmoji = await (await this.browser.findElement(this.checkBoxEmoji)).getText();
+            if (value) {
+                if (checkBoxEmoji !== "❤") {
+                    return false;
+                }
+                // expect(checkBoxEmoji).to.equal("❤");
+            } else {
+                if (checkBoxEmoji !== "✌️") {
+                    return false;
+                }
+                // expect(checkBoxEmoji).to.equal("✌️");
+            }
+            if (!isDisabled) {
+                if (!(await this.validateClick(this.checkBoxEmoji))) {
+                    return false;
+                }
+                const checkBoxEmoji = await (await this.browser.findElement(this.checkBoxEmoji)).getText();
+                if (value) {
+                    if (checkBoxEmoji !== "✌️") {
+                        return false;
+                    }
+                } else {
+                    if (checkBoxEmoji !== "❤") {
+                        return false;
+                    }
+                }
+                await this.browser.click(this.checkBoxEmoji);
+            }
+            return true;
+
+        }
+    }
+    private isDisabledTested = false;
+    public async validateDisabledCheckbox(disabled: boolean): Promise<boolean> {
+        if (disabled && (!this.isDisabledTested)) {
+            this.isDisabledTested = true;
+            (await this.browser.getALLConsoleLogs());//to delete all logs before click
+            await this.browser.click(this.checkBoxComponent);
+            if (await this.isTextPresentedInConsole(`checkbox value changed`)) {
+                return false;
+            }
+            return true;
+        }
+        return true;
+    }
+
+    public async validateClick(locator: Locator): Promise<boolean> {
+        (await this.browser.getALLConsoleLogs());//to delete all logs before click
+        await this.browser.click(locator);
+        if (!(await this.isTextPresentedInConsole(`checkbox value changed`))) {
+            return false;
+        }
+        return true;
+    }
+
+    public async testIfElementShown(locator: Locator): Promise<boolean> {
+        let foundElement: WebElement | undefined = undefined;
+        try {
+            foundElement = await this.browser.findElement(locator);
+        } catch (e: any) {
+            return false;
+        }
+        return true;
+    }
+
+    public async isTextPresentedInConsole(expectedValueInConsole: string): Promise<boolean> {
+        const consoleOutput = (await this.browser.getALLConsoleLogs()).join();
+        return consoleOutput.includes(expectedValueInConsole);
+    }
 }
+
+
