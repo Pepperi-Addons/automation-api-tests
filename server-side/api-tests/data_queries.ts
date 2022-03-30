3e7b57abfcdcda86aeaf9118dc60c81d51cdce81
PEPPERI\evgeny.v <evgeny.v@pepperi.com>
2022-03-30 13:53:39 +0300

    deleted duplicated chart manager tests

--- server-side/api-tests/data_queries.ts
+++ server-side/api-tests/data_queries.ts
@@ -69,6 +69,9 @@ export async function DataQueriesTests(generalService: GeneralService, request, 
                 });
             }
         });
+        describe('Endpoints', () => {
+            
+         });
     });
 
     //test goes here//
