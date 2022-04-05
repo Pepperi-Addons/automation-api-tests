36bc7f28ffd58f1a56cc4f98f4cd9a2075269860
OrenVildermanPepperi <oren.v@pepperi.com>
2022-04-05 14:45:26 +0300

    Update PFS version in DIMX tests and worked on CRUD of UDC

--- server-side/services/user-defined-collections.service.ts
+++ server-side/services/user-defined-collections.service.ts
@@ -1,10 +1,11 @@
-import { PapiClient } from '@pepperi-addons/papi-sdk';
+import { PapiClient, FindOptions } from '@pepperi-addons/papi-sdk';
 import GeneralService from './general.service';
 
 export class UDCService {
     papiClient: PapiClient;
     generalService: GeneralService;
     uuid: string;
+    sk: any;
 
     constructor(public service: GeneralService, uuid) {
         this.papiClient = service.papiClient;
@@ -16,25 +17,28 @@ export class UDCService {
         return this.papiClient.userDefinedCollections.schemes.upsert(body);
     }
 
-    getCollectionschemes() {
-        return this.papiClient.userDefinedCollections.schemes.find();
+    getCollectionschemes(options?: FindOptions) {
+        return this.papiClient.userDefinedCollections.schemes.find(options);
     }
 
     postDocument(collectionName, body) {
         return this.papiClient.userDefinedCollections.documents(collectionName).upsert(body);
     }
 
-    getDocuments(collectionName) {
-        return this.papiClient.userDefinedCollections.documents(collectionName).find();
+    getDocuments(collectionName, options?: FindOptions) {
+        return this.papiClient.userDefinedCollections.documents(collectionName).find(options);
     }
 
     async getCollectionFromADAL(collection) {
+        if (!this.sk) {
+            this.sk = await this.generalService.getSecretKey(this.uuid);
+        }
         return await this.generalService
             .fetchStatus(`/addons/data/${this.uuid}/${collection}`, {
                 method: 'GET',
                 headers: {
                     'X-Pepperi-OwnerID': this.uuid,
-                    'X-Pepperi-SecretKey': await this.generalService.getSecretKey(this.uuid),
+                    'X-Pepperi-SecretKey': this.sk,
                 },
             })
             .then((res) => res.Body);
