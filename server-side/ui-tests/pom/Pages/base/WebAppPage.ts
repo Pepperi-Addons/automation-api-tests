80ae1198dd53b326e5070fcc69804c0b1d786358
OferPepperi <ofer.l@pepperi.com>
2022-04-07 10:20:59 +0300

    readonly LoaderSpinner locator.

--- server-side/ui-tests/pom/Pages/base/WebAppPage.ts
+++ server-side/ui-tests/pom/Pages/base/WebAppPage.ts
@@ -4,7 +4,7 @@ import { WebAppHeader } from '../../WebAppHeader';
 import { Page } from './Page';
 
 export abstract class WebAppPage extends Page {
-    public static LoadingSpinner: By = By.css('#loadingSpinnerModal');
+    public static readonly LoadingSpinner: By = By.css('#loadingSpinnerModal');
     public Header: WebAppHeader;
     /**
      *
