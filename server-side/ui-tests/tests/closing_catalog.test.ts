29f3fc3ffa52c93219030cb7e752b073c3fbbc8a
PEPPERI\evgeny.v <evgeny.v@pepperi.com>
2022-04-28 14:20:02 +0300

    added bug verafication for DI-20093

---
+++ server-side/ui-tests/tests/closing_catalog.test.ts
@@ -0,0 +1,48 @@
+import { Client } from "@pepperi-addons/debug-server/dist";
+import { GeneralService } from "../../services";
+import { Browser } from "../utilities/browser";
+import { describe, it, afterEach, beforeEach } from 'mocha';
+import { WebAppLoginPage } from "../pom";
+import { By } from "selenium-webdriver";
+import { expect } from "chai";
+
+
+export async function CloseCatalogTest(email: string, password: string, varPass: string, client: Client) {
+    const generalService = new GeneralService(client);
+    let driver: Browser;
+
+    // await generalService.baseAddonVersionsInstallation(varPass);
+    describe('Basic UI Tests Suit', async function () {
+        this.retries(0);
+
+        beforeEach(async function () {
+            driver = await Browser.initiateChrome();
+        });
+
+        afterEach(async function () {
+            const webAppLoginPage = new WebAppLoginPage(driver);
+            await webAppLoginPage.collectEndTestData(this);
+            await driver.quit();
+        });
+
+        it('Login - Goto Sales Order And Test If Closing Catalog Create A General Error', async function () {
+            const webAppLoginPage = new WebAppLoginPage(driver);
+            const WebAppHomePage = await webAppLoginPage.login(email, password);
+            await WebAppHomePage.initiateSalesActivity(undefined, undefined, false);
+            await driver.click(WebAppHomePage.MainHomePageBtn);
+            driver.sleep(2000);
+            try {
+                const erroDialog = await driver.findElement(By.css("pep-dialog > div > span"));
+                const errorText = await erroDialog.getText();
+                expect(errorText).to.include("Error");
+                expect.fail("general error message recived after closgin catalog and returning to order center");
+            } catch (e: any) {
+                if (e.message === 'general error message recived after closgin catalog and returning to order center') {
+                    expect.fail("general error message recived after closgin catalog and returning to order center");
+                } else {
+                    return;
+                }
+            }
+        });
+    });
+}
