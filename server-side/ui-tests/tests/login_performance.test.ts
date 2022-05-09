e323838ccea4d0d83cb4f626c07ed801f142ffe4
PEPPERI\evgeny.v <evgeny.v@pepperi.com>
2022-05-09 14:00:21 +0300

    log perf test is WIP

---
+++ server-side/ui-tests/tests/login_performance.test.ts
@@ -0,0 +1,26 @@
+import { Browser } from '../utilities/browser';
+import { describe, it, afterEach, beforeEach } from 'mocha';
+import { WebAppLoginPage } from '../pom';
+
+export async function LoginTests(email: string, password: string) {
+    let driver: Browser;
+
+    describe('Basic UI Tests Suit', async function () {
+        this.retries(1);
+
+        beforeEach(async function () {
+            driver = await Browser.initiateChrome();
+        });
+
+        afterEach(async function () {
+            const webAppLoginPage = new WebAppLoginPage(driver);
+            await webAppLoginPage.collectEndTestData(this);
+            await driver.quit();
+        });
+
+        it('Login', async function () {
+            const webAppLoginPage = new WebAppLoginPage(driver);
+            await webAppLoginPage.login(email, password);
+        });
+    });
+}
