5e0a975d8d7e7b4aa62a9206134af2987555ac25
PEPPERI\evgeny.v <evgeny.v@pepperi.com>
2022-05-24 16:08:55 +0300

    fix lint

--- server-side/ui-tests/tests/test.index.ts
+++ server-side/ui-tests/tests/test.index.ts
@@ -26,7 +26,6 @@ import {
     LoginPerfTests,
 } from './index';
 import { ObjectsService } from '../../services/objects.service';
-import addContext from 'mochawesome/addContext';
 import { Client } from '@pepperi-addons/debug-server';
 import { UIControl } from '@pepperi-addons/papi-sdk';
 import { testData } from './../../services/general.service';
@@ -376,7 +375,8 @@ export async function replaceItemsTests(generalService: GeneralService) {
                         } catch (error) {
                             console.log(`POST item faild for item: ${JSON.stringify(filteredArray[j])}`);
                             console.log(
-                                `Wait ${6 * (6 - maxLoopsCounter)} seconds, and retry ${maxLoopsCounter - 1
+                                `Wait ${6 * (6 - maxLoopsCounter)} seconds, and retry ${
+                                    maxLoopsCounter - 1
                                 } more times`,
                             );
                             generalService.sleep(6000 * (6 - maxLoopsCounter));
