5f81fc78fc11a1c70e0ec0fc5c4ed16ebaa831b8
OlegYef <olegy@pepperi.com>
2022-07-19 16:15:31 +0300

    general service PAPI last version updating

--- server-side/services/general.service.ts
+++ server-side/services/general.service.ts
@@ -21,7 +21,7 @@ import tester from '../tester';
 
 export const testData = {
     'API Testing Framework': ['eb26afcd-3cf2-482e-9ab1-b53c41a6adbe', ''], //OUR TESTING ADDON
-    'Services Framework': ['00000000-0000-0000-0000-000000000a91', '9.5.498'], //PAPI
+    'Services Framework': ['00000000-0000-0000-0000-000000000a91', '9.5'], //PAPI
     'Cross Platforms API': ['00000000-0000-0000-0000-000000abcdef', '9.'],
     'WebApp API Framework': ['00000000-0000-0000-0000-0000003eba91', '16.80.7'], //CPAS //hardcoded version because there are CPAS .80 versions only for CPI team testing - this one is phased
     'WebApp Platform': ['00000000-0000-0000-1234-000000000b2b', '16.85.79'],
