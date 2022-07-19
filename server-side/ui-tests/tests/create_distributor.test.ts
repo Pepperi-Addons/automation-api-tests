b2acf8242fc2052e12c9e0841e255774208f54ae
PEPPERI\evgeny.v <evgeny.v@pepperi.com>
2022-07-19 11:51:27 +0300

    handling DI-19116: wont create various dists

--- server-side/ui-tests/tests/create_distributor.test.ts
+++ server-side/ui-tests/tests/create_distributor.test.ts
@@ -20,7 +20,7 @@ import { AddonLoadCondition } from '../pom/addons/base/AddonPage';
 import { TestDataTests } from '../../api-tests/test-service/test_data';
 import { LoginTests, OrderTests } from '.';
 import { replaceItemsTests, replaceUIControlsTests, newUserDependenciesTests } from './test.index';
-import { Key } from 'selenium-webdriver';
+import { By, Key } from 'selenium-webdriver';
 
 chai.use(promised);
 
@@ -82,49 +82,88 @@ export async function CreateDistributorTests(generalService: GeneralService, var
                     Company: distributorCompany,
                     Password: distributorPassword,
                 });
-
-                expect(newDistributor.Status).to.equal(200);
-                //TODO: Remove this when bug will be solved (DI-19115)
-                try {
-                    expect(newDistributor.Body.Status.ID, JSON.stringify(newDistributor.Body.AuditInfo)).to.equal(1);
-                } catch (error) {
-                    if (typeof newDistributor.Body.AuditInfo.ErrorMessage === 'string') {
-                        if (
-                            newDistributor.Body.Status.ID == 0 &&
-                            newDistributor.Body.AuditInfo.ErrorMessage.includes(
-                                'Failed to install the following addons',
-                            )
-                        ) {
-                            console.log('%cBug exist for this response: (DI-19115)', ConsoleColors.BugSkipped);
-                            console.log(JSON.parse(newDistributor.Body.AuditInfo.ResultObject));
+                //(DI-19116):
+                if (
+                    newDistributor.Status === 500 &&
+                    newDistributor.Body.Text.includes(
+                        'The requested URL was rejected. Please consult with your administrator',
+                    )
+                ) {
+                    generalService.sleep(1000 * 75 * 1);
+                    const adminClient = await generalService.initiateTester(clientArr[0].Email, clientArr[0].Password);
+                    const adminService = new GeneralService(adminClient);
+                    const systemAddons = await adminService.fetchStatus('/addons?page_size=-1&where=Type LIKE 1');
+                    expect(systemAddons.Body.length).to.be.above(10);
+                    const webAppLoginPage = new WebAppLoginPage(driver);
+                    await webAppLoginPage.navigate();
+                    await webAppLoginPage.signIn(clientArr[0].Email, clientArr[0].Password);
+                    const webAppHomePage = new WebAppHomePage(driver);
+                    let tryCounter = 0;
+                    let isHomePageLoaded = false;
+                    do {
+                        isHomePageLoaded = await webAppHomePage.untilIsVisible(webAppHomePage.MainHomePageBtn, 90000);
+                        if (!isHomePageLoaded) {
+                            tryCounter++;
+                            await driver.refresh();
+                            generalService.sleep(1000 * 5 * 1);
+                            const isErrorPresented = await driver.untilIsVisible(
+                                By.xpath('//span[contains(text(),"Error")]'),
+                            );
+                            if (isErrorPresented) {
+                                await driver.refresh();
+                            }
+                        }
+                    } while (!isHomePageLoaded && tryCounter < 15);
+                    await expect(webAppHomePage.untilIsVisible(webAppHomePage.MainHomePageBtn, 90000)).eventually.to.be
+                        .true;
+                    const adminAddons = await adminService.getInstalledAddons();
+                    expect(adminAddons.length).to.be.above(10);
+                } else {
+                    expect(newDistributor.Status).to.equal(200);
+                    //TODO: Remove this when bug will be solved (DI-19115)
+                    try {
+                        expect(newDistributor.Body.Status.ID, JSON.stringify(newDistributor.Body.AuditInfo)).to.equal(
+                            1,
+                        );
+                    } catch (error) {
+                        if (typeof newDistributor.Body.AuditInfo.ErrorMessage === 'string') {
+                            if (
+                                newDistributor.Body.Status.ID == 0 &&
+                                newDistributor.Body.AuditInfo.ErrorMessage.includes(
+                                    'Failed to install the following addons',
+                                )
+                            ) {
+                                console.log('%cBug exist for this response: (DI-19115)', ConsoleColors.BugSkipped);
+                                console.log(JSON.parse(newDistributor.Body.AuditInfo.ResultObject));
+                            } else {
+                                throw new Error(
+                                    `Status.ID: ${newDistributor.Status.ID}, AuditInfo.ErrorMessage: ${newDistributor.Body.AuditInfo.ErrorMessage}`,
+                                );
+                            }
                         } else {
                             throw new Error(
-                                `Status.ID: ${newDistributor.Status.ID}, AuditInfo.ErrorMessage: ${newDistributor.Body.AuditInfo.ErrorMessage}`,
+                                `Error Without Error Message: Status.ID: ${newDistributor.Status.ID}, Response Body: ${newDistributor.Body}`,
                             );
                         }
-                    } else {
-                        throw new Error(
-                            `Error Without Error Message: Status.ID: ${newDistributor.Status.ID}, Response Body: ${newDistributor.Body}`,
-                        );
                     }
+                    expect(newDistributor.Body.DistributorUUID).to.have.lengthOf(36);
+
+                    const adminClient = await generalService.initiateTester(clientArr[0].Email, clientArr[0].Password);
+                    const adminService = new GeneralService(adminClient);
+                    const systemAddons = await adminService.fetchStatus('/addons?page_size=-1&where=Type LIKE 1');
+                    const adminAddons = await adminService.getInstalledAddons();
+
+                    expect(systemAddons.Body.length).to.be.above(10);
+                    expect(adminAddons.length).to.be.above(10);
+
+                    const webAppLoginPage = new WebAppLoginPage(driver);
+                    await webAppLoginPage.navigate();
+                    await webAppLoginPage.signIn(clientArr[0].Email, clientArr[0].Password);
+
+                    const webAppHomePage = new WebAppHomePage(driver);
+                    await expect(webAppHomePage.untilIsVisible(webAppHomePage.MainHomePageBtn, 90000)).eventually.to.be
+                        .true;
                 }
-                expect(newDistributor.Body.DistributorUUID).to.have.lengthOf(36);
-
-                const adminClient = await generalService.initiateTester(clientArr[0].Email, clientArr[0].Password);
-                const adminService = new GeneralService(adminClient);
-                const systemAddons = await adminService.fetchStatus('/addons?page_size=-1&where=Type LIKE 1');
-                const adminAddons = await adminService.getInstalledAddons();
-
-                expect(systemAddons.Body.length).to.be.above(10);
-                expect(adminAddons.length).to.be.above(10);
-
-                const webAppLoginPage = new WebAppLoginPage(driver);
-                await webAppLoginPage.navigate();
-                await webAppLoginPage.signIn(clientArr[0].Email, clientArr[0].Password);
-
-                const webAppHomePage = new WebAppHomePage(driver);
-                await expect(webAppHomePage.untilIsVisible(webAppHomePage.MainHomePageBtn, 90000)).eventually.to.be
-                    .true;
             });
         });
 
