98a9ddbe4f60c6df49e449b139a00c1c124cfb5f
PEPPERI\evgeny.v <evgeny.v@pepperi.com>
2022-10-18 10:07:54 +0300

    633 version of testing framework+lock on papi 515

--- server-side/services/general.service.ts
+++ server-side/services/general.service.ts
@@ -21,7 +21,7 @@ import tester from '../tester';
 
 export const testData = {
     'API Testing Framework': ['eb26afcd-3cf2-482e-9ab1-b53c41a6adbe', ''], //OUR TESTING ADDON
-    'Services Framework': ['00000000-0000-0000-0000-000000000a91', '9.5.%'], //PAPI locked on newest
+    'Services Framework': ['00000000-0000-0000-0000-000000000a91', '9.5.515'], //PAPI locked on newest
     'Cross Platforms API': ['00000000-0000-0000-0000-000000abcdef', '9.%'],
     'WebApp API Framework': ['00000000-0000-0000-0000-0000003eba91', '16.80.22'], //CPAS //hardcoded version because there are CPAS .80 versions only for CPI team testing - this one is phased
     'WebApp Platform': ['00000000-0000-0000-1234-000000000b2b', '17.14.80'], //NG14
