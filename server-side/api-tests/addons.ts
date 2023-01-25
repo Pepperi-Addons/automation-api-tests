ce9ea4e55c2882fb8d7a7c10d577946e3904004e
PEPPERI\evgeny.v <evgeny.v@pepperi.com>
2023-01-25 17:40:56 +0200

    fix lint

--- server-side/api-tests/addons.ts
+++ server-side/api-tests/addons.ts
@@ -272,8 +272,8 @@ export async function ExecuteAddonsTests(generalService: GeneralService, request
         varKey = request.body.varKeyPro;
     }
 
-     //For local run that run on Jenkins this is needed since Jenkins dont inject SK to the test execution folder
-     if (generalService['client'].AddonSecretKey == '00000000-0000-0000-0000-000000000000') {
+    //For local run that run on Jenkins this is needed since Jenkins dont inject SK to the test execution folder
+    if (generalService['client'].AddonSecretKey == '00000000-0000-0000-0000-000000000000') {
         const addonSecretKey = await generalService.getSecretKey(generalService['client'].AddonUUID, varKey);
         generalService['client'].AddonSecretKey = addonSecretKey;
         generalService.papiClient['options'].addonSecretKey = addonSecretKey;
