1c63ca98d111e4bb4c408502aba9431350c6d057
PEPPERI\evgeny.v <evgeny.v@pepperi.com>
2023-04-09 16:34:57 +0300

    small change in survey to cleanse better

--- server-side/ui-tests/pom/Pages/WebAppLoginPage.ts
+++ server-side/ui-tests/pom/Pages/WebAppLoginPage.ts
@@ -57,7 +57,7 @@ export class WebAppLoginPage extends Page {
         await this.navigate();
         await this.signIn(email, password);
         const homePage = new WebAppHomePage(this.browser);
-        await expect(this.untilIsVisible(homePage.MainHomePageBtn, 50000)).eventually.to.be.true;
+        await expect(this.untilIsVisible(homePage.MainHomePageBtn, 90000)).eventually.to.be.true;
         console.log('Home Button Found -- HomePage Is Loaded');
         return new WebAppHomePage(this.browser);
     }
