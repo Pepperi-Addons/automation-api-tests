c9356f2ab78c5a2963daf8e4f5fd14b70c5c03e4
yonimaz <yoni.m@pepperi.com>
2023-04-30 15:28:44 +0300

    fix purge pfs

--- server-side/api-tests/pepperi_notification_service.ts
+++ server-side/api-tests/pepperi_notification_service.ts
@@ -97,7 +97,8 @@ export async function PepperiNotificationServiceTests(
                                 );
                         }
                         const newSchema = await adalService.postSchema({ Name: schemaNameArr[index] });
-                        expect(purgedSchema).to.equal('') || expect(purgedSchema).to.be.undefined; //oleg1
+                        expect(purgedSchema).to.have.property('Done').that.is.true || expect(purgedSchema).to.be.undefined; //oleg1
+                        expect(purgedSchema).to.have.property('RemovedCounter').that.is.a('number') || expect(purgedSchema).to.be.undefined; //oleg1
                         expect(newSchema).to.have.property('Name').a('string').that.is.equal(schemaNameArr[index]);
                         expect(newSchema).to.have.property('Type').a('string').that.is.equal('meta_data');
                     }
@@ -1146,7 +1147,8 @@ export async function PepperiNotificationServiceTests(
                             );
                     }
                     const newSchema = await adalService.postSchema({ Name: _SYNC_SCHEMA_NAME });
-                    expect(purgedSchema).to.equal('') || expect(purgedSchema).to.be.undefined; //oleg1
+                    expect(purgedSchema).to.have.property('Done').that.is.true || expect(purgedSchema).to.be.undefined; //oleg1
+                    expect(purgedSchema).to.have.property('RemovedCounter').that.is.a('number') || expect(purgedSchema).to.be.undefined; //oleg1
                     expect(newSchema).to.have.property('Name').a('string').that.is.equal(_SYNC_SCHEMA_NAME);
                     expect(newSchema).to.have.property('Type').a('string').that.is.equal('meta_data');
                 });
