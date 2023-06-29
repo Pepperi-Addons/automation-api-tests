57cfc7675f208634daa65630fc988bb5a6c0f563
EvgenyVendrovPepperi <evgeny.v@pepperi.com>
2023-06-29 14:35:43 +0300

    new sync versions

--- server-side/services/general.service.ts
+++ server-side/services/general.service.ts
@@ -53,7 +53,7 @@ export const testDataWithNewSync = {
     'Services Framework': ['00000000-0000-0000-0000-000000000a91', '9.6.%'], //PAPI locked on TLS 2 version
     'Cross Platforms API': ['00000000-0000-0000-0000-000000abcdef', '9.6.%'], //cpapi
     'WebApp API Framework': ['00000000-0000-0000-0000-0000003eba91', '17.10.%'], //CPAS
-    'Cross Platform Engine': ['bb6ee826-1c6b-4a11-9758-40a46acb69c5', '1.2.%'], //cpi-node (Cross Platform Engine)
+    'Cross Platform Engine': ['bb6ee826-1c6b-4a11-9758-40a46acb69c5', '1.3.%'], //cpi-node (Cross Platform Engine)
     'Cross Platform Engine Data': ['d6b06ad0-a2c1-4f15-bebb-83ecc4dca74b', ''],
     'Async Task Execution': ['00000000-0000-0000-0000-0000000a594c', '1.0.%'], // evgeny: 2/2/23 - has to be upgraded
     'Core Data Source Interface': ['00000000-0000-0000-0000-00000000c07e', ''],
