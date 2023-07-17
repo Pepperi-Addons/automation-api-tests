b4c725596e90da2b68973042d6c2acfdd8468878
EvgenyVendrovPepperi <evgeny.v@pepperi.com>
2023-07-17 10:28:03 +0300

    core is locked on 0.6.41 for survey

--- server-side/ui-tests/tests/survey.test.ts
+++ server-side/ui-tests/tests/survey.test.ts
@@ -138,7 +138,7 @@ export async function SurveyTests(email: string, password: string, client: Clien
         Nebula: ['00000000-0000-0000-0000-000000006a91', ''],
         sync: ['5122dc6d-745b-4f46-bb8e-bd25225d350a', '0.7.%'], //has to remain untouched - latest 0.7.x
         'Core Data Source Interface': ['00000000-0000-0000-0000-00000000c07e', ''],
-        'Core Resources': ['fc5a5974-3b30-4430-8feb-7d5b9699bc9f', ''],
+        'Core Resources': ['fc5a5974-3b30-4430-8feb-7d5b9699bc9f', '0.6.41'],
         'User Defined Collections': ['122c0e9d-c240-4865-b446-f37ece866c22', ''],
         'Resource List': ['0e2ae61b-a26a-4c26-81fe-13bdd2e4aaa3', ''],
         'Abstract Activity': ['92b9bd68-1660-4998-91bc-3b745b4bab11', ''],
