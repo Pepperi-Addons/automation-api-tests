f496d3e35cb7103b454354a4d61f8449e835ecc3
hagitPepperi <hagit.r@pepperi.com>
2023-07-20 16:45:41 +0300

    webapp platform back to unlimit at pricing

--- server-side/ui-tests/tests/NgxLibPOC.test.ts
+++ server-side/ui-tests/tests/NgxLibPOC.test.ts
@@ -16,8 +16,8 @@ export async function NgxLibPOC() {
     let storyBookPage: StoryBookPage;
     let colorPicker: ColorPicker;
 
-    describe('NGX_LIB POC Tests Suit', async function () {
-        await describe('Color Picker Component Testing', () => {
+    describe('NGX_LIB POC Tests Suit', function () {
+        describe('Color Picker Component Testing', () => {
             this.retries(0);
 
             before(async function () {
@@ -47,8 +47,17 @@ export async function NgxLibPOC() {
                     value: 'data:image/png;base64,' + base64ImageBuild,
                 });
             });
-            it(`2. Enter Color-Picker Component StoryBook`, async function () {
-                //2. choose a component by name
+            it(`2. Enter Table Of Contents of Storybook`, async function () {
+                //2. enter table of contents through "View Storybook" button
+                await storyBookPage.enterTableOfContents();
+                const base64ImageComponent = await driver.saveScreenshots();
+                addContext(this, {
+                    title: `Component Page We Got Into`,
+                    value: 'data:image/png;base64,' + base64ImageComponent,
+                });
+            });
+            it(`3. Enter Color-Picker Component StoryBook`, async function () {
+                //3. choose a component by name
                 await storyBookPage.chooseComponent('color-picker');
                 const base64ImageComponent = await driver.saveScreenshots();
                 addContext(this, {
@@ -56,12 +65,12 @@ export async function NgxLibPOC() {
                     value: 'data:image/png;base64,' + base64ImageComponent,
                 });
             });
-            it(`3. Test Color-Picker Component`, async function () {
-                //3. test component
-                //3.1. is shown
+            it(`4. Test Color-Picker Component`, async function () {
+                //4. test component
+                //4.1. is shown
                 const isComponentFound = await colorPicker.isComponentFound();
                 expect(isComponentFound).to.equal(true);
-                //3.2. can be opened and presents correct modal
+                //4.2. can be opened and presents correct modal
                 await colorPicker.openComonentModal();
                 const base64ImageComponentModal = await driver.saveScreenshots();
                 addContext(this, {
@@ -71,23 +80,23 @@ export async function NgxLibPOC() {
                 const isComponentModalFullyShown = await colorPicker.isModalFullyShown();
                 expect(isComponentModalFullyShown).to.equal(true);
                 await colorPicker.okModal();
-                //3.3. can change label
+                //4.3. can change label
                 const newLabelToSet = 'evgeny';
                 await storyBookPage.inputs.changeLabel(newLabelToSet);
                 const newLabelGotFromUi = await colorPicker.getLabel();
                 expect(newLabelGotFromUi).to.equal(newLabelToSet);
-                //3.4. can disabled
+                //4.4. can disabled
                 await storyBookPage.inputs.toggleDissableComponent();
                 const isPenIconFound = await colorPicker.isPenIconFound();
                 expect(isPenIconFound).to.equal(false);
                 await storyBookPage.inputs.toggleDissableComponent();
-                //3.5. can AACompilent on/off -- TODO: what does it mean?
-                //3.6. showTitle
+                //4.5. can AACompilent on/off -- TODO: what does it mean?
+                //4.6. showTitle
                 await storyBookPage.inputs.toggleShowTitle();
                 const labelAfterDisabelingTitle = await colorPicker.getLabel();
                 expect(labelAfterDisabelingTitle).to.equal('Type is main'); // once there is no title - the next title is the one 'taken'
                 await storyBookPage.inputs.toggleShowTitle();
-                //3.7. run on all type and see ->3.2.
+                //4.7. run on all type and see ->3.2.
                 const allTypes = await storyBookPage.inputs.getAllTypes();
                 for (let index = 0; index < allTypes.length; index++) {
                     const type = allTypes[index];
@@ -96,11 +105,11 @@ export async function NgxLibPOC() {
                     expect(isComponentModalFullyShown).to.equal(true);
                 }
                 await allTypes[0].click();
-                //3.8. color value
+                //4.8. color value
                 await storyBookPage.inputs.setColorValue('#1fbeb9');
                 const currentColor = await colorPicker.getComponentColor();
                 expect(currentColor).to.equal('(31, 190, 185)'); // same as "#1fbeb9" in RGB
-                //3.9. xAlignment
+                //4.9. xAlignment
                 const currentAlign = await colorPicker.getComponentTxtAlignment();
                 expect(currentAlign).to.include('left');
                 const allAlignments = await storyBookPage.inputs.getAllAlignments();
@@ -112,8 +121,8 @@ export async function NgxLibPOC() {
                     expect(currentAlign).to.include(alignExpectedValues[index]);
                 }
             });
-            it(`4. Test All Color-Picker Stories`, async function () {
-                //4. test all stories
+            it(`5. Test All Color-Picker Stories`, async function () {
+                //5. test all stories
                 const allStories = await colorPicker.getAllStories();
                 for (let index = 0; index < allStories.length; index++) {
                     const storyComponent = allStories[index];
@@ -129,122 +138,83 @@ export async function NgxLibPOC() {
                 }
             });
         });
-        // describe('Border Radius Component Testing', async () => {
-        //     // const driver: Browser = await Browser.initiateChrome();
-        //     this.retries(0);
+        describe('Border Radius Component Testing', async () => {
+            // const driver: Browser = await Browser.initiateChrome();
+            this.retries(0);
 
-        //     // before(async function () {
-        //     //     driver = await Browser.initiateChrome();
-        //     //     webAppHomePage = new WebAppHomePage(driver);
-        //     //     storyBookPage = new StoryBookPage(driver);
-        //     //     colorPicker = new ColorPicker(driver);
-        //     // });
+            before(async function () {
+                driver = await Browser.initiateChrome();
+                webAppHomePage = new WebAppHomePage(driver);
+                storyBookPage = new StoryBookPage(driver);
+                colorPicker = new ColorPicker(driver);
+            });
 
-        //     after(async function () {
-        //         await driver.quit();
-        //     });
+            after(async function () {
+                await driver.quit();
+            });
 
-        //     afterEach(async function () {
-        //         await webAppHomePage.collectEndTestData(this);
-        //     });
-        //     it(`1. Enter Story Book Site & Choose Latest Master Build`, async function () {
-        //         //1. navigate to story book webstite
-        //         await storyBookPage.navigate();
-        //         //1.1. choose the latest build
-        //         await storyBookPage.chooseLatestBuild();
-        //         //1.2. document on which build were running
-        //         const base64ImageBuild = await driver.saveScreenshots();
-        //         addContext(this, {
-        //             title: `Were Running On This Build Of StoryBook`,
-        //             value: 'data:image/png;base64,' + base64ImageBuild,
-        //         });
-        //     });
-        //     it(`5. Enter Border-Radius Component StoryBook`, async function () {
-        //         // Navigate to Storybook Home Page
-        //         // await driver.navigate(storybook_homeURL);
-        //         // await driver.switchToDefaultContent
-        //         // await storyBookPage.isSpinnerDone();
-        //         //5. choose a component by name
-        //         await storyBookPage.chooseComponent('border-radius');
-        //         const base64ImageComponent = await driver.saveScreenshots();
-        //         addContext(this, {
-        //             title: `Component Page We Got Into`,
-        //             value: 'data:image/png;base64,' + base64ImageComponent,
-        //         });
-        //     });
-        //     // it(`6. Test Border-Radius Component`, async function () {
-        //     //     //6. test component
-        //     //     //6.1. is shown
-        //     //     const isComponentFound = await colorPicker.isComponentFound();
-        //     //     expect(isComponentFound).to.equal(true);
-        //     //     //6.2. can be opened and presents correct modal
-        //     //     await colorPicker.openComonentModal();
-        //     //     const base64ImageComponentModal = await driver.saveScreenshots();
-        //     //     addContext(this, {
-        //     //         title: `Presented Component Modal`,
-        //     //         value: 'data:image/png;base64,' + base64ImageComponentModal,
-        //     //     });
-        //     //     const isComponentModalFullyShown = await colorPicker.isModalFullyShown();
-        //     //     expect(isComponentModalFullyShown).to.equal(true);
-        //     //     await colorPicker.okModal();
-        //     //     //6.3. can change label
-        //     //     const newLabelToSet = 'evgeny';
-        //     //     await storyBookPage.inputs.changeLabel(newLabelToSet);
-        //     //     const newLabelGotFromUi = await colorPicker.getLabel();
-        //     //     expect(newLabelGotFromUi).to.equal(newLabelToSet);
-        //     //     //6.4. can disabled
-        //     //     await storyBookPage.inputs.toggleDissableComponent();
-        //     //     const isPenIconFound = await colorPicker.isPenIconFound();
-        //     //     expect(isPenIconFound).to.equal(false);
-        //     //     await storyBookPage.inputs.toggleDissableComponent();
-        //     //     //6.5. can AACompilent on/off -- TODO: what does it mean?
-        //     //     //6.6. showTitle
-        //     //     await storyBookPage.inputs.toggleShowTitle();
-        //     //     const labelAfterDisabelingTitle = await colorPicker.getLabel();
-        //     //     expect(labelAfterDisabelingTitle).to.equal('Type is main'); // once there is no title - the next title is the one 'taken'
-        //     //     await storyBookPage.inputs.toggleShowTitle();
-        //     //     //6.7. run on all type and see ->6.2.
-        //     //     const allTypes = await storyBookPage.inputs.getAllTypes();
-        //     //     for (let index = 0; index < allTypes.length; index++) {
-        //     //         const type = allTypes[index];
-        //     //         await type.click();
-        //     //         const isComponentModalFullyShown = await colorPicker.testComponentModal();
-        //     //         expect(isComponentModalFullyShown).to.equal(true);
-        //     //     }
-        //     //     await allTypes[0].click();
-        //     //     //6.8. color value
-        //     //     await storyBookPage.inputs.setColorValue('#1fbeb9');
-        //     //     const currentColor = await colorPicker.getComponentColor();
-        //     //     expect(currentColor).to.equal('(31, 190, 185)'); // same as "#1fbeb9" in RGB
-        //     //     //6.9. xAlignment
-        //     //     const currentAlign = await colorPicker.getComponentTxtAlignment();
-        //     //     expect(currentAlign).to.include('left');
-        //     //     const allAlignments = await storyBookPage.inputs.getAllAlignments();
-        //     //     const alignExpectedValues = ['center', 'right'];
-        //     //     for (let index = 0; index < allAlignments.length; index++) {
-        //     //         const alignment = allAlignments[index];
-        //     //         await alignment.click();
-        //     //         const currentAlign = await colorPicker.getComponentTxtAlignment();
-        //     //         expect(currentAlign).to.include(alignExpectedValues[index]);
-        //     //     }
-        //     // });
-        //     // it(`7. Test All Border-Radius Stories`, async function () {
-        //     //     //7. test all stories
-        //     //     const allStories = await colorPicker.getAllStories();
-        //     //     for (let index = 0; index < allStories.length; index++) {
-        //     //         const storyComponent = allStories[index];
-        //     //         await storyComponent.click();
-        //     //         const isComponentModalFullyShown = await colorPicker.isModalFullyShown();
-        //     //         const story64ImageComponent = await driver.saveScreenshots();
-        //     //         addContext(this, {
-        //     //             title: `Story Modal As Presented In StoryBook`,
-        //     //             value: 'data:image/png;base64,' + story64ImageComponent,
-        //     //         });
-        //     //         expect(isComponentModalFullyShown).to.equal(true);
-        //     //         await colorPicker.okModal();
-        //     //     }
-        //     // });
-
-        // });
+            afterEach(async function () {
+                await webAppHomePage.collectEndTestData(this);
+            });
+            it(`1. Enter Story Book Site & Choose Latest Master Build`, async function () {
+                //1. navigate to story book webstite
+                await storyBookPage.navigate();
+                //1.1. choose the latest build
+                await storyBookPage.chooseLatestBuild();
+                //1.2. document on which build were running
+                const base64ImageBuild = await driver.saveScreenshots();
+                addContext(this, {
+                    title: `Were Running On This Build Of StoryBook`,
+                    value: 'data:image/png;base64,' + base64ImageBuild,
+                });
+            });
+            it(`2. Enter Table Of Contents of Storybook`, async function () {
+                //2. enter table of contents through "View Storybook" button
+                await storyBookPage.enterTableOfContents();
+                const base64ImageComponent = await driver.saveScreenshots();
+                addContext(this, {
+                    title: `Component Page We Got Into`,
+                    value: 'data:image/png;base64,' + base64ImageComponent,
+                });
+            });
+            it(`5. Enter Border-Radius Abstract StoryBook`, async function () {
+                // Navigate to Storybook Home Page
+                // await driver.navigate(storybook_homeURL);
+                // await driver.switchToDefaultContent
+                // await storyBookPage.isSpinnerDone();
+                //5. choose a component by name
+                await storyBookPage.chooseAbstract('border-radius');
+                let base64ImageComponent = await driver.saveScreenshots();
+                addContext(this, {
+                    title: `Component Page We Got Into`,
+                    value: 'data:image/png;base64,' + base64ImageComponent,
+                });
+                await driver.click(storyBookPage.CanvasTab);
+                driver.sleep(1 * 1000);
+                await driver.switchTo(storyBookPage.StorybookIframe);
+                await driver.untilIsVisible(storyBookPage.BorderRadiusContainer);
+                await driver.untilIsVisible(storyBookPage.BorderRadiusInnerContainer);
+                const borderRadiusSM_element = await driver.findElement(storyBookPage.BorderRadiusSM);
+                const borderRadiusMD_element = await driver.findElement(storyBookPage.BorderRadiusMD);
+                const borderRadiusL_element = await driver.findElement(storyBookPage.BorderRadiusL);
+                const borderRadiusXL_element = await driver.findElement(storyBookPage.BorderRadiusXL);
+                const borderRadiusXL_color = await borderRadiusXL_element.getCssValue('color');
+                const borderRadiusSM_border_radius = await borderRadiusSM_element.getCssValue('border-radius');
+                const borderRadiusMD_border_radius = await borderRadiusMD_element.getCssValue('border-radius');
+                const borderRadiusL_border_radius = await borderRadiusL_element.getCssValue('border-radius');
+                const borderRadiusXL_border_radius = await borderRadiusXL_element.getCssValue('border-radius');
+                base64ImageComponent = await driver.saveScreenshots();
+                addContext(this, {
+                    title: `Component Page We Got Into`,
+                    value: 'data:image/png;base64,' + base64ImageComponent,
+                });
+                console.info('borderRadiusXL_color: ', JSON.stringify(borderRadiusXL_color, null, 2));
+                console.info('borderRadiusSM_border_radius: ', JSON.stringify(borderRadiusSM_border_radius, null, 2));
+                console.info('borderRadiusMD_border_radius: ', JSON.stringify(borderRadiusMD_border_radius, null, 2));
+                console.info('borderRadiusL_border_radius: ', JSON.stringify(borderRadiusL_border_radius, null, 2));
+                console.info('borderRadiusXL_border_radius: ', JSON.stringify(borderRadiusXL_border_radius, null, 2));
+                // debugger
+            });
+        });
     });
 }
