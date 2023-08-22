49379d1659a46d51afea0a7253f3d543be2ccd00
EvgenyVendrovPepperi <evgeny.v@pepperi.com>
2023-08-22 11:56:02 +0300

    sync: versions to work on buyer

--- server-side/ui-tests/tests/sync.test.ts
+++ server-side/ui-tests/tests/sync.test.ts
@@ -41,7 +41,7 @@ export async function SyncTests(email: string, password: string, client: Client,
     const userInfoCollectionName = 'UserInfo';
     const userInfoCollectionSize = 1000;
     let driver: Browser;
-    await generalService.baseAddonVersionsInstallation(varPass);
+    // await generalService.baseAddonVersionsInstallation(varPass);//---> has to get 1.0.X which is NOT avaliable
     // #region Upgrade survey dependencies
 
     const testData = {
@@ -52,7 +52,7 @@ export async function SyncTests(email: string, password: string, client: Client,
         Nebula: ['00000000-0000-0000-0000-000000006a91', ''],
         sync: ['5122dc6d-745b-4f46-bb8e-bd25225d350a', '0.7.%'], //has to remain untouched - latest 0.7.x
         'Core Data Source Interface': ['00000000-0000-0000-0000-00000000c07e', ''],
-        'Core Resources': ['fc5a5974-3b30-4430-8feb-7d5b9699bc9f', ''],
+        // 'Core Resources': ['fc5a5974-3b30-4430-8feb-7d5b9699bc9f', ''],//---> has to get 1.0.X which is NOT avaliable
         'User Defined Collections': ['122c0e9d-c240-4865-b446-f37ece866c22', ''],
         'Resource List': ['0e2ae61b-a26a-4c26-81fe-13bdd2e4aaa3', ''],
         Slugs: ['4ba5d6f9-6642-4817-af67-c79b68c96977', ''],
@@ -603,7 +603,9 @@ export async function SyncTests(email: string, password: string, client: Client,
                     await webAppHomePage.clickOnBtn('Accounts');
                     generalService.sleep(1000 * 5);
                     const accountName = accountNamesArray[index];
-                    console.log(`Testing Account: ${accountName}, Number ${index + 1} Out Of ${accountNamesArray.length}`);
+                    console.log(
+                        `Testing Account: ${accountName}, Number ${index + 1} Out Of ${accountNamesArray.length}`,
+                    );
                     await webAppList.searchInList(accountName);
                     await webAppList.clickOnLinkFromListRowWebElement(0);
                     const eseUtils = new E2EUtils(driver);
@@ -658,7 +660,9 @@ export async function SyncTests(email: string, password: string, client: Client,
                     await webAppHomePage.clickOnBtn('Accounts');
                     generalService.sleep(1000 * 5);
                     const accountName = accountNamesArray[index];
-                    console.log(`Testing Account: ${accountName}, Number ${index + 1} Out Of ${accountNamesArray.length}`);
+                    console.log(
+                        `Testing Account: ${accountName}, Number ${index + 1} Out Of ${accountNamesArray.length}`,
+                    );
                     await webAppList.searchInList(accountName);
                     await webAppList.clickOnLinkFromListRowWebElement(0);
                     const eseUtils = new E2EUtils(driver);
