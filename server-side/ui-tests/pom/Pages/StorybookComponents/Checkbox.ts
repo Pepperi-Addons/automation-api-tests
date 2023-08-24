d39fea2d1629979e87264b4284d4d346e1c037b7
hagitPepperi <hagit.r@pepperi.com>
2023-08-24 16:39:16 +0300

    storybook checkbox - input tests setup

--- server-side/ui-tests/pom/Pages/StorybookComponents/Checkbox.ts
+++ server-side/ui-tests/pom/Pages/StorybookComponents/Checkbox.ts
@@ -2,9 +2,14 @@ import { By } from 'selenium-webdriver';
 import { StorybookComponent } from './Base/StorybookComponent';
 
 export class Checkbox extends StorybookComponent {
-    public ModalOKBtn: By = By.xpath(`//span[contains(text(),'Ok')]`);
+    public MainExampleLabel: By = By.xpath(``);
 
     public async doesCheckboxComponentFound(): Promise<void> {
         await this.doesComponentFound('checkbox', 'Checkbox');
+    }
+
+    public async getLabel(): Promise<string> {
+        const label = await this.browser.findElement(this.MainExampleLabel);
+        return await label.getText();
     }
 }
