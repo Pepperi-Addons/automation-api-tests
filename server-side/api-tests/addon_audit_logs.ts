98457ef8923c840efa35b569f320fdf684ca707f
yonimaz <yoni.m@pepperi.com>
2023-11-28 09:59:29 +0200

    fix lint

--- server-side/api-tests/addon_audit_logs.ts
+++ server-side/api-tests/addon_audit_logs.ts
@@ -452,7 +452,7 @@ export async function AddonAuditLogsTests(generalService: GeneralService, reques
                         Authorization: `Bearer ${generalService['client'].OAuthAccessToken}`,
                         //X-Pepperi-OwnerID is the ID of the Addon
                         'X-Pepperi-OwnerID': addonUUID,
-                        'X-Pepperi-SecretKey': await generalService.getSecretKey(addonUUID, varKey)
+                        'X-Pepperi-SecretKey': await generalService.getSecretKey(addonUUID, varKey),
                     },
                 });
 
