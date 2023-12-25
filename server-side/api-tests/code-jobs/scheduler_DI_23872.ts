8147a9184ec9f931eaf260d68eaceefb3346e650
EvgenyVendrovPepperi <evgeny.v@pepperi.com>
2023-12-25 16:17:58 +0200

    schduler part 2 (stop & restsrt are ready)

--- server-side/api-tests/code-jobs/scheduler_DI_23872.ts
+++ server-side/api-tests/code-jobs/scheduler_DI_23872.ts
@@ -13,7 +13,7 @@ export async function SchedulerTests_Part2(generalService: GeneralService, reque
     const CallbackCash: any = {};
     let CodeJobBody: any = {};
     let CodeJobUUIDCron;
-    let AfterStop = 0;
+    let codeJobExecuteResponse;
 
     let varKey;
     if (generalService.papiClient['options'].baseURL.includes('staging')) {
@@ -21,10 +21,10 @@ export async function SchedulerTests_Part2(generalService: GeneralService, reque
     } else {
         varKey = request.body.varKeyPro;
     }
-    // await generalService.baseAddonVersionsInstallation(varKey, undefined, true);
+    await generalService.baseAddonVersionsInstallation(varKey, undefined, true);
     const testData = {
-        // Scheduler: ['8bc903d1-d97a-46b8-990b-50bea356e35b', ''],
-        // 'Async Task Execution': ['00000000-0000-0000-0000-0000000a594c', ''],
+        Scheduler: ['8bc903d1-d97a-46b8-990b-50bea356e35b', ''],
+        'Async Task Execution': ['00000000-0000-0000-0000-0000000a594c', ''],
     };
 
     const chnageVersionResponseArr = await generalService.changeVersion(varKey, testData, false);
@@ -34,9 +34,9 @@ export async function SchedulerTests_Part2(generalService: GeneralService, reque
     const addonUUID = generalService['client'].BaseURL.includes('staging')
         ? '48d20f0b-369a-4b34-b48a-ffe245088513'
         : '78696fc6-a04f-4f82-aadf-8f823776473f';
-    const jsFileName = 'test_functions.js'; //'test.js';
-    const functionNamecreateNewCodeJobRetryTest = 'scheduler';
-    const version = '0.0.5';
+    const jsFileName = 'test.js';
+    const functionNamecreateNewCodeJobRetryTest = 'createNewCodeJobRetryTest';
+    const version = '0.0.4';
 
     describe('Cron Expression Test Case', async function () {
         describe('Prerequisites Addons for Scheduler Tests', () => {
@@ -90,20 +90,21 @@ export async function SchedulerTests_Part2(generalService: GeneralService, reque
                 ).Version;
                 console.log(`installed version ${installedVersion} of testing addon ${addonUUID}`);
             });
-            it(`2. Create A Job Based On Pepperitest Addon Version 0.0.5 To Run Every 2 Minutes`, async () => {
+            it(`2. Create A Job Based On Pepperitest Addon Version 0.0.4 - Not Schesuled With 10 Retries`, async () => {
                 CodeJobBody = {
                     UUID: '',
                     CodeJobName: 'New CodeJob with IsScheduled true',
                     Description: 'Cron verification',
                     Owner: '',
-                    CronExpression: '*/2 * * * *',
+                    CronExpression: '',
                     NextRunTime: null,
-                    IsScheduled: true,
+                    IsScheduled: false,
                     Type: 'AddonJob',
                     CodeJobIsHidden: false,
                     CreationDateTime: '',
                     ModificationDateTime: '',
                     ExecutionMemoryLevel: 1,
+                    NumberOfTries: 10,
                     AddonPath: jsFileName, // Only for AddonJob
                     AddonUUID: addonUUID, // Only for AddonJob
                     FunctionName: functionNamecreateNewCodeJobRetryTest,
@@ -115,54 +116,49 @@ export async function SchedulerTests_Part2(generalService: GeneralService, reque
                     })
                 ).Body;
                 expect(CallbackCash.insertNewCJtoCronVerification.CodeJobName).to.equal(CodeJobBody.CodeJobName);
-                expect(codeJobResponse.CodeJobIsHidden).to.equal(false);
-                expect(codeJobResponse.IsScheduled).to.equal(true);
+                expect(codeJobResponse.CodeJobIsHidden).to.equal(CodeJobBody.CodeJobIsHidden);
+                expect(codeJobResponse.IsScheduled).to.equal(CodeJobBody.IsScheduled);
                 expect(codeJobResponse.CronExpression).to.equal(CodeJobBody.CronExpression);
+                expect(codeJobResponse.NumberOfTries).to.equal(CodeJobBody.NumberOfTries);
                 CodeJobUUIDCron = CallbackCash.insertNewCJtoCronVerification.UUID;
             });
-            it(`3. Get New Code Job Execution Response For At Least Two Runs`, async () => {
-                generalService.sleep(130000);
-                CallbackCash.ResponseEmptyExecutedLogsCronTest = await service.auditLogs.find({
-                    where: `AuditInfo.JobMessageData.CodeJobUUID='${CodeJobUUIDCron}'`,
-                });
-                expect(CallbackCash.ResponseEmptyExecutedLogsCronTest.length).to.equal(1);
-                expect(CallbackCash.ResponseEmptyExecutedLogsCronTest[0].Status.Name).to.equal('Success');
-                expect(
-                    JSON.parse(CallbackCash.ResponseEmptyExecutedLogsCronTest[0].AuditInfo.ResultObject).success,
-                ).to.equal(true);
-                expect(
-                    JSON.parse(CallbackCash.ResponseEmptyExecutedLogsCronTest[0].AuditInfo.ResultObject).errorMessage,
-                ).to.equal('test msg');
-                expect(
-                    JSON.parse(
-                        JSON.parse(CallbackCash.ResponseEmptyExecutedLogsCronTest[0].AuditInfo.ResultObject)
-                            .resultObject.multiplyResult,
-                    ),
-                ).to.equal(6);
-                generalService.sleep(128000);
-                CallbackCash.ResponseEmptyExecutedLogsCronTest = await service.auditLogs.find({
-                    where: `AuditInfo.JobMessageData.CodeJobUUID='${CodeJobUUIDCron}'`,
-                });
-                expect(CallbackCash.ResponseEmptyExecutedLogsCronTest.length).to.be.at.least(2);
-                for (let index = 0; index < CallbackCash.ResponseEmptyExecutedLogsCronTest.length; index++) {
-                    expect(CallbackCash.ResponseEmptyExecutedLogsCronTest[index].Status.Name).to.equal('Success');
-                    expect(
-                        JSON.parse(CallbackCash.ResponseEmptyExecutedLogsCronTest[index].AuditInfo.ResultObject)
-                            .success,
-                    ).to.equal(true);
-                    expect(
-                        JSON.parse(CallbackCash.ResponseEmptyExecutedLogsCronTest[index].AuditInfo.ResultObject)
-                            .errorMessage,
-                    ).to.equal('test msg');
-                    expect(
-                        JSON.parse(
-                            JSON.parse(CallbackCash.ResponseEmptyExecutedLogsCronTest[index].AuditInfo.ResultObject)
-                                .resultObject.multiplyResult,
-                        ),
-                    ).to.equal(6);
-                }
+            it(`3. Excute The  Job`, async () => {
+                codeJobExecuteResponse = (
+                    await generalService.fetchStatus(
+                        '/code_jobs/async/' + CallbackCash.insertNewCJtoCronVerification.UUID + '/execute',
+                        {
+                            method: 'POST',
+                            body: JSON.stringify({}),
+                        },
+                    )
+                ).Body;
+                expect(codeJobExecuteResponse).to.haveOwnProperty('URI');
+                expect(codeJobExecuteResponse).to.haveOwnProperty('ExecutionUUID');
+                console.log(
+                    `got execution uuid for async run on job ${CallbackCash.insertNewCJtoCronVerification.UUID}: ${codeJobExecuteResponse.ExecutionUUID}`,
+                );
             });
-            it(`4. Stop The Code Job`, async () => {
+            it(`4. See It Returns A Run Entry On Audit Log - Poll Untill Its Status Is 4 => InRetry`, async () => {
+                let codeJobResponse = await generalService.fetchStatus(
+                    '/audit_logs/' + codeJobExecuteResponse.ExecutionUUID,
+                    { method: 'GET' },
+                );
+                while (codeJobResponse.Body.Status.ID !== 4) {
+                    generalService.sleep(1000);
+                    codeJobResponse = await generalService.fetchStatus(
+                        '/audit_logs/' + codeJobExecuteResponse.ExecutionUUID,
+                        { method: 'GET' },
+                    );
+                }
+                expect(codeJobResponse.Body.Status.ID).to.equal(4);
+                expect(codeJobResponse.Body.Status.Name).to.equal('InRetry');
+                expect(codeJobResponse.Body.AuditInfo.JobMessageData.NumberOfTry).to.equal(2);
+                expect(codeJobResponse.Body.AuditInfo.JobMessageData.NumberOfTries).to.equal(10);
+                expect(JSON.parse(codeJobResponse.Body.AuditInfo.ResultObject).errorMessage).to.equal(
+                    'Failed due to exception: Nofar Test',
+                );
+            });
+            it(`4. Stop The Code Job And Get It To See That: numOfTry = numOfTries+1`, async () => {
                 const bodyToSend = { KeyList: [CodeJobUUIDCron] };
                 const codeJobResponse = await generalService.fetchStatus('/addons/jobs/stop', {
                     method: 'POST',
@@ -171,19 +167,26 @@ export async function SchedulerTests_Part2(generalService: GeneralService, reque
                 expect(codeJobResponse.Ok).to.equal(true);
                 expect(codeJobResponse.Status).to.equal(200);
                 expect(codeJobResponse.Body).to.deep.equal({});
+                const codeJobGet = await service.auditLogs.find({
+                    where: `AuditInfo.JobMessageData.CodeJobUUID='${CodeJobUUIDCron}'`,
+                });
+                expect(codeJobGet[0].AuditInfo.JobMessageData.NumberOfTry).to.equal(
+                    codeJobGet[0].AuditInfo.JobMessageData.NumberOfTries + 1,
+                );
             });
-            it(`5. Wait For 4.5 Minutes (Stopping Is Not Immediate) - Then See It Doesn't Run Anymore For 4 Minutes`, async () => {
-                generalService.sleep(270000);
-                CallbackCash.ResponseEmptyExecutedLogsCronTest = await service.auditLogs.find({
+            it(`5. Wait For 3 Minutes - And See The Job Execution Is 'Frozen'`, async () => {
+                generalService.sleep(180000);
+                const codeJobResponse = await service.auditLogs.find({
                     where: `AuditInfo.JobMessageData.CodeJobUUID='${CodeJobUUIDCron}'`,
                 });
-                const prevNumOfRuns = CallbackCash.ResponseEmptyExecutedLogsCronTest.length;
-                generalService.sleep(240000);
-                CallbackCash.ResponseEmptyExecutedLogsCronTest = await service.auditLogs.find({
-                    where: `AuditInfo.JobMessageData.CodeJobUUID='${CodeJobUUIDCron}'`,
-                });
-                expect(CallbackCash.ResponseEmptyExecutedLogsCronTest.length).to.equal(prevNumOfRuns);
-                AfterStop = CallbackCash.ResponseEmptyExecutedLogsCronTest.length;
+                expect(codeJobResponse.length).to.equal(1);
+                expect((codeJobResponse[0] as any).Status.ID).to.equal(4);
+                expect((codeJobResponse[0] as any).Status.Name).to.equal('InRetry');
+                expect((codeJobResponse[0] as any).AuditInfo.JobMessageData.NumberOfTry).to.equal(2);
+                expect((codeJobResponse[0] as any).AuditInfo.JobMessageData.NumberOfTries).to.equal(10);
+                expect(JSON.parse((codeJobResponse[0] as any).AuditInfo.ResultObject).errorMessage).to.equal(
+                    'Failed due to exception: Nofar Test',
+                );
             });
             it(`6. Call Restart On This Job`, async () => {
                 const bodyToSend = { KeyList: [CodeJobUUIDCron] };
@@ -195,19 +198,29 @@ export async function SchedulerTests_Part2(generalService: GeneralService, reque
                 expect(codeJobResponse.Status).to.equal(200);
                 expect(codeJobResponse.Body).to.deep.equal({});
             });
-            it(`7. Wait For 5 Minutes & See The Job Is Back Running`, async () => {
-                generalService.sleep(300000);
+            it(`7. Wait For 2 Minutes & See The Job Is Back Running`, async () => {
+                generalService.sleep(160000);
                 CallbackCash.ResponseEmptyExecutedLogsCronTest = await service.auditLogs.find({
                     where: `AuditInfo.JobMessageData.CodeJobUUID='${CodeJobUUIDCron}'`,
                 });
-                expect(CallbackCash.ResponseEmptyExecutedLogsCronTest.length).to.be.above(AfterStop);
+                expect(
+                    CallbackCash.ResponseEmptyExecutedLogsCronTest[0].AuditInfo.JobMessageData.NumberOfTry,
+                ).to.be.above(2);
+                const prevNumOfTry =
+                    CallbackCash.ResponseEmptyExecutedLogsCronTest[0].AuditInfo.JobMessageData.NumberOfTry;
+                generalService.sleep(160000);
+                CallbackCash.ResponseEmptyExecutedLogsCronTest = await service.auditLogs.find({
+                    where: `AuditInfo.JobMessageData.CodeJobUUID='${CodeJobUUIDCron}'`,
+                });
+                expect(
+                    CallbackCash.ResponseEmptyExecutedLogsCronTest[0].AuditInfo.JobMessageData.NumberOfTry,
+                ).to.be.above(prevNumOfTry);
             });
-            it(`LAST: Clean Code Job`, async () => {
+            it(`8. Hide The Specific Code Job`, async () => {
                 CodeJobBody = {
                     UUID: CodeJobUUIDCron,
-                    IsScheduled: false,
+                    CodeJobIsHidden: true,
                 };
-
                 CallbackCash.updateCronToChroneTestIsScheduledFalse = await generalService.fetchStatus('/code_jobs', {
                     method: 'POST',
                     body: JSON.stringify(CodeJobBody),
@@ -215,6 +228,19 @@ export async function SchedulerTests_Part2(generalService: GeneralService, reque
                 expect(CallbackCash.updateCronToChroneTestIsScheduledFalse.Status).to.equal(200);
                 expect(CallbackCash.updateCronToChroneTestIsScheduledFalse.Ok).to.equal(true);
                 expect(CallbackCash.updateCronToChroneTestIsScheduledFalse.Body.UUID).to.equal(CodeJobUUIDCron);
+                expect(CallbackCash.updateCronToChroneTestIsScheduledFalse.Body.CodeJobIsHidden).to.equal(true);
+                // const allCodeJobs = await generalService.fetchStatus('/code_jobs?page_size=-1', { method: "GET" });
+                // const allTestCodeJobs = allCodeJobs.Body.filter(codeJob => codeJob.SupportAdminUUID === 'a8f60b30-0066-41fd-8a56-2f43fb5ab8b0' &&codeJob.Type === 'AddonJob');
+                // for (let index = 0; index < allTestCodeJobs.length; index++) {
+                //     const cj = allTestCodeJobs[index];
+                //     const bodyToSend = { UUID: cj.UUID, CodeJobIsHidden: true };
+                //     const codeJobHideResponse = await generalService.fetchStatus('/code_jobs', { method: "POST", body:JSON.stringify(bodyToSend)});
+                //     expect(codeJobHideResponse.Body.UUID).to.equal(cj.UUID);
+                //     expect(codeJobHideResponse.Body.CodeJobIsHidden).to.equal(true);
+                // }
+                // const allCodeJobsAfterHidding = await generalService.fetchStatus('/code_jobs?page_size=-1', { method: "GET" });
+                // const allTestCodeJobsAfterHidding = allCodeJobs.Body.filter(codeJob => codeJob.SupportAdminUUID === 'a8f60b30-0066-41fd-8a56-2f43fb5ab8b0' &&codeJob.Type === 'AddonJob');
+                // debugger;
             });
         });
     });
