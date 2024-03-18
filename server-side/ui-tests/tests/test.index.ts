b195898c048d800a884fefb0709f5ead8369a290
hagitPepperi <hagit.r@pepperi.com>
2024-03-18 13:21:23 +0200

    nelt test - switch adding items from non-bundle to bundle

--- server-side/ui-tests/tests/test.index.ts
+++ server-side/ui-tests/tests/test.index.ts
@@ -142,7 +142,7 @@ const whichAddonToUninstall = process.env.npm_config_which_addon as string;
 const XForSyncTimes = Number(process.env.npm_config_x as any);
 // const neltTestParams = process.env.npm_config_nelt_test_params as string;
 const burgerMenuVisitFlowName = process.env.npm_config_nelt_burger_vf_name as string;
-const visitFlowName = process.env.npm_config_nelt_vf_name as string;
+const divisionName = process.env.npm_config_nelt_division_name as string;
 const nearExpiryVisitGroupName = process.env.npm_config_nelt_expiry_vf_group as string;
 const nearExpiryVisitStepName = process.env.npm_config_nelt_expiry_vf_step as string;
 const orderOfReturnsVisitGroupName = process.env.npm_config_nelt_returns_vf_group as string;
@@ -160,7 +160,7 @@ const nonBundlePromotionString = process.env.npm_config_nelt_promotion as string
 const nonPromotionItemsString = process.env.npm_config_nelt_items as string;
 // const nonPromotionItemsIndices = process.env.npm_config_nelt_items_indices ? JSON.parse(process.env.npm_config_nelt_items_indices as string) as number[] : undefined;
 // const nonPromotionItemsQuantities = process.env.npm_config_nelt_items_qty ? JSON.parse(process.env.npm_config_nelt_items_qty as string) as number[] : undefined;
-// const nonBundlePromotionItemsIndices = process.env.npm_config_nelt_promotion_indices ? JSON.parse(process.env.npm_config_nelt_promotion_indices as string) as number[] : undefined;
+// const bundlePromotionItemsIndices = process.env.npm_config_nelt_promotion_indices ? JSON.parse(process.env.npm_config_nelt_promotion_indices as string) as number[] : undefined;
 
 (async function () {
     const tempGeneralService = new GeneralService({
@@ -846,7 +846,7 @@ const nonPromotionItemsString = process.env.npm_config_nelt_items as string;
             pass,
             client,
             burgerMenuVisitFlowName,
-            visitFlowName,
+            divisionName,
             nearExpiryVisitGroupName,
             nearExpiryVisitStepName,
             orderOfReturnsVisitGroupName,
