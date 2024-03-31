89b98addc6d9368a1be11330773fb7990d22643c
hagitPepperi <hagit.r@pepperi.com>
2024-03-31 12:38:10 +0300

    TestDataTests added to NeltPerformanceTests

--- server-side/ui-tests/tests/test.index.ts
+++ server-side/ui-tests/tests/test.index.ts
@@ -886,6 +886,7 @@ const nonPromotionItemsString = process.env.npm_config_nelt_items as string;
             // nonPromotionItemsQuantities,
             // nonBundlePromotionItemsIndices,
         );
+        await TestDataTests(generalService, { describe, expect, it } as TesterFunctions);
         run();
         return;
     }
