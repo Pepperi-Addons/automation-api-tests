5e160517d9adcefdbcb091a6568c9671abaddc73
EvgenyVendrovPepperi <evgeny.v@pepperi.com>
2024-05-15 14:30:35 +0300

    locking nebula dists to work with slugs 1.3.7

--- server-side/services/general.service.ts
+++ server-side/services/general.service.ts
@@ -76,9 +76,10 @@ export const testDataNoSyncNoNebula = {
 };
 
 const baseAddonsForNebula = { ...testData };
+baseAddonsForNebula.Slugs = ['4ba5d6f9-6642-4817-af67-c79b68c96977', '1.3.7'];
 baseAddonsForNebula.configurations = ['84c999c3-84b7-454e-9a86-71b7abc96554', '0.7.%'];
-baseAddonsForNebula['Cross Platform Engine Data'] = ['d6b06ad0-a2c1-4f15-bebb-83ecc4dca74b', '0.6.20'];
-baseAddonsForNebula.sync = ['5122dc6d-745b-4f46-bb8e-bd25225d350a', '1.0.%'];
+baseAddonsForNebula['Cross Platform Engine Data'] = ['d6b06ad0-a2c1-4f15-bebb-83ecc4dca74b', '0.6.%'];
+baseAddonsForNebula.sync = ['5122dc6d-745b-4f46-bb8e-bd25225d350a', '1.%.%'];
 export const testDataForNebulaDists = {
     ...baseAddonsForNebula,
     Nebula: ['00000000-0000-0000-0000-000000006a91', ''],
