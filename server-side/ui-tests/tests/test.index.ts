6792beafa3b6e372c2eb801bb0b480f25edc5f83
EvgenyVendrovPepperi <evgeny.v@pepperi.com>
2024-05-23 16:29:26 +0300

    added some printing to second step ADAL cicd

--- server-side/ui-tests/tests/test.index.ts
+++ server-side/ui-tests/tests/test.index.ts
@@ -1388,7 +1388,7 @@ const udcContainedArray = process.env.npm_config_udc_contained_array as string;
     }
 
     if (tests === 'second_part_adal_cli') {
-        console.log('\n################ RUNNING FIRST TEST: Adal Big Data ################\n');
+        console.log('\n################ RUNNING FIRST TEST OUT OF 2: Adal Big Data ################\n');
         await AdalBigDataTestser(
             generalService,
             {
@@ -1400,7 +1400,7 @@ const udcContainedArray = process.env.npm_config_udc_contained_array as string;
             },
             { describe, expect, it } as TesterFunctions,
         );
-        console.log('\n################ RUNNING SECOND TEST: Doc DB Indexed Adal ################\n');
+        console.log('\n################ RUNNING SECOND TEST OUT OF 2: Doc DB Indexed Adal ################\n');
         await DocDBIndexedAdalTestser(
             generalService,
             {
