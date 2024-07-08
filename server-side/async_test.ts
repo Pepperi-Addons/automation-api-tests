386507756b5028d34fc000821ab3bb15e6b618fa
OlegYef <olegy@pepperi.com>
2024-07-08 12:18:43 +0300

    async_test updated

--- server-side/async_test.ts
+++ server-side/async_test.ts
@@ -8,7 +8,8 @@ export async function async_test(client: Client, request: Request) {
     ret['success'] = false;
     console.log('Message from Callback first');
     console.log('OLEG: ' + JSON.stringify(request)); // added to verify request
-    if (request.body.Result.resultObject.msg == 'hello world') {
+    //if (request.body.Result.resultObject.msg == 'hello world') {
+    if (request.body.Response.resultObject.msg == 'hello world') {
         ret['success'] = true;
         ret['resultObject'].msg = 'hello world - returned from callback function';
     } else {
