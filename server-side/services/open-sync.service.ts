a362dde715a664bd980360744aa7391fe43330c3
EvgenyVendrovPepperi <evgeny.v@pepperi.com>
2024-08-22 10:03:03 +0300

    survey returned to sync 1 + open sync loggging

--- server-side/services/open-sync.service.ts
+++ server-side/services/open-sync.service.ts
@@ -21,6 +21,7 @@ export class OpenSyncService {
             ModificationDateTime: modificationDateTime, //'1970-11-23T14:39:50.781Z',
             Format: 'JSON',
         };
+        console.log(`==> sent body is: ${JSON.stringify(bodyToSend)}`);
         const openSyncResponse = await this.generalService.fetchStatus(
             '/addons/api/5122dc6d-745b-4f46-bb8e-bd25225d350a/open-sync/sync',
             {
@@ -28,6 +29,7 @@ export class OpenSyncService {
                 body: JSON.stringify(bodyToSend),
             },
         );
+        console.log(`==> response from '/open-sync/sync' is: ${JSON.stringify(openSyncResponse)}`);
         return openSyncResponse;
     }
 }
